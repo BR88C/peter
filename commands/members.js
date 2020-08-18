ead9d3d59a556be417d5ffd6a1a298dc3a048c68
BR88C <bcroethel88@gmail.com>
2020-08-17 21:53:28 -0400

    Create members.js

---
+++ commands/members.js
@@ -0,0 +1,29 @@
+const Discord = require(`discord.js`);
+
+module.exports = {
+	name: `members`,
+	description: `Lists the number of members in your server.`,
+	guildOnly: true,
+	cooldown: 0,
+	aliases: [`users`],
+	async execute(client, message, args) {
+    var memberCount = message.guild.memberCount;
+    var memberPeople = message.guild.members.cache.filter(member => !member.user.bot).size;
+    var memberBot = message.guild.members.cache.filter(member => member.user.bot).size;
+    var memberOnline = message.guild.members.cache.filter(member => member.presence.status === 'online' || member.presence.status === 'dnd').size;
+	
+	let memberEmbed = new Discord.MessageEmbed()
+		.setColor(0xacecb6)
+		.setAuthor(`Guild Member Information`)
+		.setTitle(`**Total Guild Members**`)
+		.setDescription(memberCount)
+		.addFields(
+			{ name: '**Total Bots:**', value: memberBot },
+			{ name: '**Total People:**', value: memberPeople },
+			{ name: '**Total Online:**', value: memberOnline },
+		)
+		
+	
+    message.channel.send(memberEmbed);
+	},
+};
