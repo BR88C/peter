ff103feb2c00d71ee6d98b2e84cec591286e1abc
BR88C <bcroethel88@gmail.com>
2020-08-18 00:25:36 -0400

    Create volume.js

---
+++ commands/volume.js
@@ -0,0 +1,28 @@
+const Discord = require(`discord.js`);
+const config = require("../config.json");
+
+module.exports = {
+	name: `volume`,
+	description: `Sets the volume for the music`,
+	guildOnly: true,
+	aliases: [`v`],
+	usage: `[volume %]`,
+	async execute(client, message, args) {
+		const { channel } = message.member.voice;
+		if(!channel) return message.channel.send(`I'm sorry but you need to be in a voice channel to change volume!`);
+
+		const serverQueue = message.client.queue.get(message.guild.id);
+
+		if(!serverQueue) return message.channel.send(`There is nothing playing.`);
+		if(!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+		
+		if((config.BR88C.id !== message.author.id) && parseInt(args[0]) > 10000 || parseInt(args[0]) <= 0) {
+			return message.reply(`volume must be between 1 and 10,000%!`);
+		}
+
+		serverQueue.volume = args[0];
+		serverQueue.connection.dispatcher.setVolume(parseInt(args[0]) / 500);
+
+		return message.channel.send(`🔊 Set the volume to: **${args[0]}%**`);
+	},
+};
