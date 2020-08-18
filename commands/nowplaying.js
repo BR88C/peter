f01e4e3cfa274e41f82aa143124d5bec0aaaca8c
BR88C <bcroethel88@gmail.com>
2020-08-18 00:24:43 -0400

    Create nowplaying.js

---
+++ commands/nowplaying.js
@@ -0,0 +1,13 @@
+const Discord = require(`discord.js`);
+
+module.exports = {
+	name: `nowplaying`,
+	description: `Replies with the song currently playing.`,
+	guildOnly: true,
+	aliases: [`np`],
+	async execute(client, message, args) {
+        const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send(`There is nothing playing.`);
+		return message.channel.send(`🎶 Now playing: **${serverQueue.songs[0].title}**`);
+	},
+};
