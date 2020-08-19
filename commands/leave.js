7487295d1689a4ca9ca3e35bc3e0ade2864fbced
BR88C <bcroethel88@gmail.com>
2020-08-19 10:45:12 -0400

    fixed bugs

--- commands/leave.js
+++ commands/leave.js
@@ -7,12 +7,19 @@ module.exports = {
 	async execute(client, message, args) {
 		const { channel } = message.member.voice;
 
-		if (!channel) return message.channel.send(`I'm sorry but you need to be in a voice channel to disconnect me!`);
+		if (!channel) return message.reply(`I'm sorry but you need to be in a voice channel to disconnect me!`);
+		const serverQueue = message.client.queue.get(message.guild.id);
 
-		if(channel) serverQueue.connection.dispatcher.destroy();
-		message.client.queue.delete(message.guild.id);
-        message.member.voice.channel.leave();
-        message.channel.send(`👋 Left the VC.`);
-        
+		if(message.guild.voice.connection) {
+			if(serverQueue) serverQueue.connection.dispatcher.destroy();
+			serverQueue.songs = [];
+			message.client.queue.delete(message.guild.id);
+			message.member.voice.channel.leave();
+			message.channel.send(`👋 Left the VC.`);
+		} else {
+			serverQueue.songs = [];
+			message.client.queue.delete(message.guild.id);
+			message.reply(`I can\'t leave if I\'m not in a VC!`);
+		}
 	},
 };
--- commands/stop.js
+++ commands/stop.js
@@ -7,14 +7,20 @@ module.exports = {
 	async execute(client, message, args) {
 		const { channel } = message.member.voice;
 
-		if (!channel) return message.channel.send(`I'm sorry but you need to be in a voice channel to stop music!`);
+		if (!channel) return message.reply(`I'm sorry but you need to be in a voice channel to stop music!`);
 		const serverQueue = message.client.queue.get(message.guild.id);
 
-		if (!serverQueue) return message.channel.send(`There is nothing playing that I could stop for you.`);
+		if (!serverQueue) return message.reply(`There is nothing playing that I could stop for you.`);
 
-		if(channel) serverQueue.connection.dispatcher.destroy();
-		serverQueue.songs = [];
-		message.client.queue.delete(message.guild.id);
-		message.channel.send(`🛑 Music stopped.`);
+		if(message.guild.voice.connection) {
+			serverQueue.connection.dispatcher.destroy();
+			serverQueue.songs = [];
+			message.client.queue.delete(message.guild.id);
+			message.channel.send(`Queue cleared and Music stopped. 🛑`);
+		} else {
+			serverQueue.songs = [];
+			message.client.queue.delete(message.guild.id);
+			message.reply(`I'm not in a VC, so there is no music to stop!`);
+		}
 	},
 };
