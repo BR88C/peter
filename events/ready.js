9b6180775fc8d4292d758306a21ea1331c48d02f
BR88C <bcroethel88@gmail.com>
2020-10-29 19:22:37 -0400

    fix member count displayed on ready event

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,6 @@ module.exports = (client) => {
     client.user.setActivity('A kid\'s guide to the Internet', { type: 'WATCHING' });
     console.log(``);
     console.log('\x1b[32m', `Using prefix ${client.config.get('prefix')}`);
-    console.log(`\x1b[32m`, `Ready! Serving ${client.guilds.cache.size} servers, ${client.channels.cache.size} channels and ${client.users.cache.size} users!`);
+    console.log(`\x1b[32m`, `Ready! Serving ${client.guilds.cache.size} servers, ${client.channels.cache.size} channels and ${client.users.cache.reduce((a, g) => a + g.memberCount, 0)} users!`);
 	console.log(`\x1b[32m`, `Running version ${client.pjson.get('version')}\n`);
 }
