ec3a6aa3307c0ca0008c9dae649572a0ca93da1b
BR88C <bcroethel88@gmail.com>
2020-10-29 21:13:26 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -6,7 +6,7 @@ const Discord = require(`discord.js`);
 module.exports = (client, message) => {
 	// If the message is in a Server
 	if(message.guild !== null) {
-		if (message.author.bot) {
+		if(message.author.bot) {
 			// If the Message is by Peter!
 			if(message.author.id === client.user.id) {
 				// If the message has an embed it logs it as an embed
@@ -31,7 +31,7 @@ module.exports = (client, message) => {
 		}
 	// If the message is in a DM
 	} else {
-		if (message.author.bot) {
+		if(message.author.bot) {
 			// If the Message is by Peter!
 			if(message.author.id === client.user.id) {
 				// If the message has an embed it logs it as an embed
@@ -60,7 +60,7 @@ module.exports = (client, message) => {
 	const commandName = args.shift().toLowerCase();
 	const command = client.commands.get(commandName)
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-	if (!command) {
+	if(!command) {
 		// If command does not exist it logs it in red
 		if(message.guild !==null) {
 			console.log(`\x1b[31m`,(`Server: ${message.guild.name} | Channel: #${message.channel.name} | [${message.author.tag}] ${message.content}`).replace(/[^ -~]+/g, ""));
@@ -78,17 +78,17 @@ module.exports = (client, message) => {
 		}
 	}
 	// Checks if command is Guild Only
-	if (command.guildOnly && message.channel.type === `dm`) {
+	if(command.guildOnly && message.channel.type === `dm`) {
 		return message.reply(`I can't execute that command inside DMs!`);
 	}
 	// Checks if command is Dev Only
-	if (command.devOnly && message.author.id !== client.config.get('dev').id) {
+	if(command.devOnly && message.author.id !== client.config.get('dev').id) {
 		return message.reply(`that command is only for this bot's dev, ${client.config.get('dev').tag}!`);
 	}
 	// Chck if command needs args
-	if (command.args && !args.length) {
+	if(command.args && !args.length) {
 		let reply = `You didn't provide any arguments, ${message.author}!`;
-		if (command.usage) {
+		if(command.usage) {
 			reply += `\nThe proper usage would be: \`${client.config.get('prefix')}${command.name} ${command.usage}\``;
 		}
 		return message.channel.send(reply);
@@ -96,15 +96,15 @@ module.exports = (client, message) => {
 
     // Cooldown
     const cooldowns = new Discord.Collection();
-	if (!cooldowns.has(command.name)) {
+	if(!cooldowns.has(command.name)) {
 		cooldowns.set(command.name, new Discord.Collection());
 	}
 	const now = Date.now();
 	const timestamps = cooldowns.get(command.name);
 	const cooldownAmount = (command.cooldown || 0) * 1000;
-	if (timestamps.has(message.author.id)) {
+	if(timestamps.has(message.author.id)) {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-		if (now < expirationTime) {
+		if(now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
 			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before executing the \`${command.name}\` command again.`);
 		}
