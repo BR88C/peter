94f99662e29f29264eea75986661763abff3f8c8
BR88C <bcroethel88@gmail.com>
2020-10-30 15:59:48 -0400

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -7,23 +7,12 @@ module.exports = {
 	cooldown: 0,
 	aliases: [`users`],
 	async execute(client, message, args) {
-		// Grabs needed variables
-		var memberCount = message.guild.memberCount;
-		var memberPeople = message.guild.members.cache.filter(member => !member.user.bot).size;
-		var memberBot = message.guild.members.cache.filter(member => member.user.bot).size;
-		var memberOnline = message.guild.members.cache.filter(member => member.presence.status === 'online' || member.presence.status === 'dnd').size;
-		
-		// Creates and sends the embed
 		let memberEmbed = new Discord.MessageEmbed()
 			.setColor(0xacecb6)
 			.setAuthor(`Guild Member Information`)
 			.setTitle(`**Total Guild Members**`)
-			.setDescription(memberCount)
-			.addFields(
-				{ name: '**Total Bots:**', value: memberBot },
-				{ name: '**Total People:**', value: memberPeople },
-				{ name: '**Total Online:**', value: memberOnline },
-			)
+			.setDescription(message.guild.memberCount)
+
 		message.channel.send(memberEmbed);
 	},
 }
