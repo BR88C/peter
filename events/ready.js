06e76702cd6072fd597e81d7c6643556e441f1d4
BR88C <bcroethel88@gmail.com>
2020-10-31 14:13:57 -0400

    update stats shown when bot is ready

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,14 @@ module.exports = (client) => {
     client.user.setActivity(`A kid's guide to the Internet`, { type: 'WATCHING' });
     console.log(``);
     console.log('\x1b[32m', `Using prefix ${client.config.get('prefix')}`);
-    console.log(`\x1b[32m`, `Ready! Serving ${client.guilds.cache.size} servers, ${client.channels.cache.size} channels and ${client.users.cache.size} users!`);
-	console.log(`\x1b[32m`, `Running version ${client.pjson.get('version')}\n`);
+    console.log(`\x1b[32m`, `Running version ${client.pjson.get('version')}\n`);
+
+    async function readyStats () {
+        var guilds = await client.guilds.cache.size;
+        var channels = await client.channels.cache.size;
+        var users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
+        console.log(`\x1b[32m`, `Ready! Serving ${guilds} servers, ${channels} channels and ${users} users!`);
+    }
+    readyStats();
+    
 }
