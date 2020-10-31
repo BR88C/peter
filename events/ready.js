63204a811ea1242f2b7caee9db16804c95c9a928
BR88C <bcroethel88@gmail.com>
2020-10-31 17:58:47 -0400

    change newline on startup

--- events/ready.js
+++ events/ready.js
@@ -6,13 +6,13 @@ module.exports = (client) => {
     client.user.setActivity(`A kid's guide to the Internet`, { type: 'WATCHING' });
     console.log(``);
     console.log('\x1b[32m', `Using prefix ${client.config.get('prefix')}`);
-    console.log(`\x1b[32m`, `Running version ${client.pjson.get('version')}\n`);
+    console.log(`\x1b[32m`, `Running version ${client.pjson.get('version')}`);
 
     async function readyStats () {
         var guilds = await client.guilds.cache.size;
         var channels = await client.channels.cache.size;
         var users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
-        console.log(`\x1b[32m`, `Ready! Serving ${guilds} servers, ${channels} channels and ${users} users!`);
+        console.log(`\x1b[32m`, `Ready! Serving ${guilds} servers, ${channels} channels and ${users} users!\n`);
     }
     readyStats();
     
