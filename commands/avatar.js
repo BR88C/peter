4faec7a8fcc2e4f0123d8a48c8ca17406ca7c773
BR88C <bcroethel88@gmail.com>
2020-11-01 15:04:53 -0500

    add gif support for avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,12 +8,18 @@ module.exports = {
     aliases: [`pfp`],
     usage: `[@user]`,
 	async execute(client, message, args) {
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+          
         const user = message.mentions.users.first() || message.author;
 
         let avatarEmbed = new Discord.MessageEmbed()
-            .setColor(0xfb94ff)
+            .setColor(getRandomInt(1,16777215))
             .setTitle(`${user.username}'s Avatar`)
-            .setImage(user.avatarURL());
+            .setImage(user.avatarURL({ dynamic: true, size: 512 }));
 
         message.channel.send(avatarEmbed);
 	},
