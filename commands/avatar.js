f6b136fcc6c3d4b07d02c15a0df4b2fa67899771
BR88C <bcroethel88@gmail.com>
2020-11-10 21:26:15 -0500

    avatar embed color based off of user id

--- commands/avatar.js
+++ commands/avatar.js
@@ -18,7 +18,7 @@ module.exports = {
         const user = message.mentions.users.first() || message.author;
 
         let avatarEmbed = new Discord.MessageEmbed()
-            .setColor(getRandomInt(1,16777215))
+            .setColor(user.id % 16777215)
             .setTitle(`${user.username}'s Avatar`)
             .setDescription(`Links: [128](${user.avatarURL({ dynamic: true, size: 128})}) \| [256](${user.avatarURL({ dynamic: true, size: 256})}) \| [512](${user.avatarURL({ dynamic: true, size: 512})}) \| [1024](${user.avatarURL({ dynamic: true, size: 1024})}) \| [2048](${user.avatarURL({ dynamic: true, size: 2048})}) `)
             .setImage(user.avatarURL({ dynamic: true, size: 512}));
