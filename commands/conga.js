40866ea0cf3fce043920fa74e897474e0e7d1e41
BR88C <bcroethel88@gmail.com>
2020-11-11 14:17:56 -0500

    add conga command

---
+++ commands/conga.js
@@ -0,0 +1,22 @@
+const Discord = require(`discord.js`);
+
+module.exports = {
+	name: `conga`,
+	description: `Hidden command that plays Conga by Gloria Estefan and sends a gif`,
+	guildOnly: true,
+	hide: true,
+	async execute(client, message, args) {
+        
+
+        await message.channel.send(`It's conga time!`, { files: ["./images/conga.gif"] })
+        
+        const playCommand = client.commands.get(`play`);
+        args = [`https://www.youtube.com/watch?v=5SXX-pWzOY8`]
+        try {
+            playCommand.execute(client, message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+	},
+}
