7786950dd78a4f70c71c622414ebb7582c544e01
BR88C <bcroethel88@gmail.com>
2020-11-11 15:30:48 -0500

    add support server, ping, and uptime to bot command

--- commands/bot.js
+++ commands/bot.js
@@ -6,6 +6,17 @@ module.exports = {
     category: `Bot Info`,
 	aliases: [`about`],
 	async execute(client, message, args) {
+        const hours = Math.floor(client.uptime / 60 / 60);
+		const minutes = Math.floor(client.uptime / 60) - (hours * 60);
+		const seconds = client.uptime % 60;
+		var uptime;
+		if(hours > 0) {
+			uptime = hours.toString() + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+		} else {
+			uptime = minutes.toString()+ ':' + seconds.toString().padStart(2, '0');
+		}
+
+
         let botInfoEmbed = new Discord.MessageEmbed()
             .setColor(0xffd87d)
             .setTitle(`Information about Peter!`)
@@ -15,6 +26,9 @@ module.exports = {
                 { name: `**Number of Commands**`, value: client.commands.size, inline: true },
                 { name: `**Prefix**`, value: client.config.get('prefix'), inline: true },
                 { name: `**Author**`, value: client.config.get('dev').tag, inline: true },
+                { name: `**Ping**`, value: `${Math.round(100*client.ws.ping)/100}ms`, inline: true },
+                { name: `**Uptime**`, value: uptime, inline: true },
+                { name: `**Support Server**`, value: client.config.get('supportServer'), inline: true },
                 { name: `**Repository**`, value: client.pjson.get('repository').url.replace(`git+`, ``), inline: true },
                 { name: `**Version**`, value: client.pjson.get('version'), inline: true }
             )
