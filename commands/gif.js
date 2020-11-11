4cb82173a99c02754051707c35658f9f0862f87a
BR88C <bcroethel88@gmail.com>
2020-11-10 22:00:34 -0500

    remove unneeded logging

--- commands/gif.js
+++ commands/gif.js
@@ -11,9 +11,6 @@ module.exports = {
 	cooldown: 3,
 	usage: `<search query>`,
 	async execute(client, message, args) {
-        // Logs that its starting the gif post process
-        console.log(`\x1b[33m`, `\n    [STARTING GIF SEARCH]`);
-
         // Sets up the random integer function
         function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -23,7 +20,6 @@ module.exports = {
 
         // Gets the search query based off of arguments
         giphysearch = args.slice(0).join(` `);
-        console.log('\x1b[33m', `   Search query: ${giphysearch}`);
         
         // Searchs giphy for [search query]
         giphy.search('gifs', {"q": `${giphysearch}`})
@@ -35,9 +31,6 @@ module.exports = {
                 const responseFinal = response.data[responseIndex];
                 const imageURL = responseFinal.images.fixed_height.url;
 
-                // Prints the final url in the console log
-                console.log('\x1b[33m', `   URL: ${imageURL}`);
-
                 // Creates Embed with Gif, "Here ya go!" heading and a random color
                 let gifEmbed = new Discord.MessageEmbed()
                     .setColor(getRandomInt(1,16777215))
@@ -46,16 +39,10 @@ module.exports = {
                     .setFooter(`Images courtesy of giphy`)
                     .setTimestamp(new Date());
                 
-                // Logs that the embed is created
-                console.log(`\x1b[33m`,`   Created Embed`);
-
-                // Logs that the message is done then posts it"
-                console.log(`\x1b[32m`, `   [FINISHED GIF SEARCH`);
                 message.channel.send(gifEmbed);
 
                 // If there is an error with the code, Giphy, or if it couldnt find the image based on the query it sends an error message
                 }).catch(() => {
-                    console.log(`\x1b[31m`,`   [ERROR] Unable to send, create, or find an image for the gif post.\n`);
                     message.reply(`Looks like I couldn't get a gif based on your search query... sorry :tired_face:`);
                 })
 	},
