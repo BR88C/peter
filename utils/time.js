f0a94f27483839a9cc4a659d480f8dfcb78e3d25
BR88C <bcroethel88@gmail.com>
2020-11-11 20:39:11 -0500

    Create time.js

---
+++ utils/time.js
@@ -0,0 +1,13 @@
+module.exports = (time) => {
+    const hours = Math.floor(time / 60 / 60);
+	const minutes = Math.floor(time / 60) - (hours * 60);
+	const seconds = time % 60;
+	var formattedTime;
+	if(hours > 0) {
+		formattedTime = hours.toString() + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+	} else {
+		formattedTime = minutes.toString()+ ':' + seconds.toString().padStart(2, '0');
+    }
+    
+    return formattedTime;
+}
