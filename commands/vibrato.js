e7394f18765b9d8495cb00b12ebd5392fa93999a
BR88C <bcroethel88@gmail.com>
2020-11-12 15:02:25 -0500

    Update vibrato.js

--- commands/vibrato.js
+++ commands/vibrato.js
@@ -18,8 +18,8 @@ module.exports = {
 		if(!args[0]) return message.channel.send(`The current vibrato level is: **${serverQueue.vibrato}%**`);
 		
 		// Checks to make sure the vibrato value specified is greater or equal to 0 and less or equal to 100
-		if(isNaN(parseInt(args[0]))) return message.reply(`please specify an Integer!`);
 		const specifiedVibrato = parseInt(args[0]);
+		if(isNaN(specifiedVibrato)) return message.reply(`please specify an Integer!`);
 		if(specifiedVibrato > 100 || specifiedVibrato < 0) {
 			return message.reply(`vibrato value must be between 0 and 100%!`);
 		}
@@ -28,7 +28,7 @@ module.exports = {
 		serverQueue.vibrato = args[0]
 
 		// Push the song at current time
-		serverQueue.songs.unshift(serverQueue.songs[0])
+		serverQueue.songs.unshift(serverQueue.songs[0]);
 		serverQueue.songs[1].startTime = serverQueue.connection.dispatcher.streamTime/1000;
 		serverQueue.songs[1].hidden = true;
 		serverQueue.connection.dispatcher.end();
