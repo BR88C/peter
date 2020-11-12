f075f250e38f1ca95b94e42c693623ae37a86480
BR88C <bcroethel88@gmail.com>
2020-11-12 15:01:53 -0500

    Update bass.js

--- commands/bass.js
+++ commands/bass.js
@@ -19,8 +19,8 @@ module.exports = {
 		if(!args[0]) return message.channel.send(`The current bass level is: **${serverQueue.bass}%**`);
 		
 		// Checks to make sure the bass value specified is greater or equal to 0 and less or equal to 100
-		if(isNaN(parseInt(args[0]))) return message.reply(`please specify an Integer!`);
 		const specifiedBass = parseInt(args[0]);
+		if(isNaN(specifiedBass)) return message.reply(`please specify an Integer!`);
 		if(specifiedBass > 100 || specifiedBass < 0) {
 			return message.reply(`bass value must be between 0 and 100%!`);
 		}
@@ -29,7 +29,7 @@ module.exports = {
 		serverQueue.bass = args[0]
 
 		// Push the song at current time
-		serverQueue.songs.unshift(serverQueue.songs[0])
+		serverQueue.songs.unshift(serverQueue.songs[0]);
 		serverQueue.songs[1].startTime = serverQueue.connection.dispatcher.streamTime/1000;
 		serverQueue.songs[1].hidden = true;
 		serverQueue.connection.dispatcher.end();
