168190c4b85f8767497996955de83d41b6fdbf8c
BR88C <bcroethel88@gmail.com>
2020-11-13 17:26:55 -0500

    show song length and progress

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,5 +1,7 @@
 const Discord = require(`discord.js`);
 const log = require(`../utils/log.js`);
+const time = require(`../utils/time.js`)
+const progressbar = require(`../utils/progressbar.js`);
 
 module.exports = {
 	name: `nowplaying`,
@@ -13,14 +15,16 @@ module.exports = {
 		// If the queue is empty
 		if (!serverQueue) return message.channel.send(`There is nothing playing.`);
 
+		const percentComplete = (serverQueue.connection.dispatcher.streamTime / 1000) / serverQueue.songs[0].rawTime;
+
 		// Create embed
 		let nowPlayingEmbed = new Discord.MessageEmbed()
 			.setColor(0xb0ffe2)
 			.setAuthor(`🎶 Currently playing:`)
 			.setTitle(`**${serverQueue.songs[0].title}**`)
 			.setURL(serverQueue.songs[0].url)
-			.setDescription(`Song Length: ${serverQueue.songs[0].timestamp}`)
-			.setImage(serverQueue.songs[0].thumbnail)
+			.setDescription(time(Math.round(serverQueue.connection.dispatcher.streamTime / 1000)) + ` ` + progressbar(percentComplete, 35) + ` ` + serverQueue.songs[0].timestamp)
+			.setThumbnail(serverQueue.songs[0].thumbnail)
 			.setFooter(`Requested by: ${serverQueue.songs[0].requestedBy.tag}`)
 			.setTimestamp(new Date());
 
