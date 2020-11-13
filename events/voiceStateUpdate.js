d6306b07000475a1ef1c21a5f0d3a868d8f9e2ea
BR88C <bcroethel88@gmail.com>
2020-11-13 16:24:42 -0500

    move serverQueue to be defined only once

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -8,12 +8,13 @@ module.exports = (client, oldState, newState) => {
 		.setColor(0xff4a4a)
 		.setTitle(`👋 Left due to no other users being present in\nthe VC, or due to being manually disconnected.`)
 
+	const serverQueue = client.queue.get(oldState.guild.id);
+
 	// If a user leaves or changes channels
 	if(oldState.channelID != newState.channelID && oldState.channelID != null) {
 		const channelInfo = oldState.guild.channels.cache.get(oldState.channelID);
 		// If the bot is the only user in the VC clear the queue and leave
 		if(channelInfo.members.has(client.user.id) && channelInfo.members.size == 1) {
-			const serverQueue = client.queue.get(oldState.guild.id);
 			if(serverQueue) {
 				serverQueue.textChannel.send(leaveEmbed);
 				if(serverQueue.connection.dispatcher) serverQueue.connection.dispatcher.destroy();
@@ -22,7 +23,6 @@ module.exports = (client, oldState, newState) => {
 			channelInfo.leave();
 		// If the bot is manually disconnected clear the queue
 		} else if(!channelInfo.members.has(client.user.id)) {
-			const serverQueue = client.queue.get(oldState.guild.id);
 			if(serverQueue) {
 				serverQueue.textChannel.send(leaveEmbed);
 				if(client.queue) client.queue.delete(oldState.guild.id);
