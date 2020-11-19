0b72bdfde52182ee6754483901ed6d5225883931
BR88C <bcroethel88@gmail.com>
2020-11-19 11:00:03 -0500

    remove cooldown

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,7 +5,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Shows ping in Milliseconds.',
 	category: `Bot Info`,
-	cooldown: 3,
 	async execute(client, message, args) {
 		// Get ping
 		ping = Math.round(100*client.ws.ping)/100;
