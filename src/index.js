026a8d17e6c3367409a818b16027e6ee564beb75
BR88C <bcroethel88@gmail.com>
2020-11-20 16:41:47 -0500

    add loader.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
 /* Peter by BR88C */
 
-/* Load Modules and Config */
+/* Load Modules */
 const Discord = require(`discord.js`);
-const fs = require(`fs`);
-const dotenv = require(`dotenv`).config();
-const config = require(`./config.json`);
-const pjson = require(`../package.json`);
 const DBL = require("dblapi.js");
+const dotenv = require(`dotenv`).config();
+const loader = require(`./modules/loader.js`)
 const log = require(`./modules/log.js`);
 
 const client = new Discord.Client();
 const dbl = new DBL(process.env.DBL_TOKEN, client);
-client.queue = new Map();
-client.config = new Map(Object.entries(config));
-client.pjson = new Map(Object.entries(pjson));
 
 
-
-/* Load events */
-const eventFiles = fs.readdirSync(`./src/events`).filter(file => file.endsWith(`.js`) && !file.startsWith(`_`));
-eventFiles.forEach(file => {
-   	const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    log(`Loaded event ${eventName}`, `yellow`);
-   	client.on(eventName, event.bind(null, client));
-});
-
-
-
-/* Load commands */
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync(`./src/commands`).filter(file => file.endsWith(`.js`) && !file.startsWith(`_`));
-commandFiles.forEach(file => {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-	log(`Loaded command ${command.name}`, `yellow`);
-})
-
-
-
-/* Authenticate the bot with client */
+/* Load all commands, events, and variables, then authenticate with Discord */
+loader.loadAll(client);
 client.login(process.env.BOT_TOKEN).catch(error => {
     log(`\nFailed to authenticate client with application.`, `red`)
     log(``, `white`);
@@ -47,13 +20,10 @@ client.login(process.env.BOT_TOKEN).catch(error => {
 });
 
 
-
-/* Post server count on top.gg */
-dbl.on('posted', () => {})
+/* Report if there is an error with DBL */
 dbl.on('error', error => {
     log(`Error with DBL API: ${error}`, `red`);
 })
-
 
 
 /* If the Bot is Stopped with Ctrl+C */
---
+++ src/modules/loader.js
@@ -0,0 +1,43 @@
+/* Loads commands, events, and variables */
+
+const Discord = require(`discord.js`);
+const fs = require(`fs`);
+const log = require(`./log.js`);
+
+module.exports =  {
+    /* Load variables and save on client */
+    async loadVariables(client) {
+        client.queue = new Map();
+        client.config = require(`../config.json`);
+        client.pjson = require(`../../package.json`);
+    },
+
+    /* Load events */
+    async loadEvents(client) {
+        const eventFiles = fs.readdirSync(`./src/events`).filter(file => file.endsWith(`.js`) && !file.startsWith(`_`));
+        eventFiles.forEach(file => {
+            const event = require(`../events/${file}`);
+            let eventName = file.split(".")[0];
+            log(`Loaded event ${eventName}`, `yellow`);
+            client.on(eventName, event.bind(null, client));
+        });
+    },
+
+    /* Load commands */
+    async loadCommands(client) {
+        client.commands = new Discord.Collection();
+        const commandFiles = fs.readdirSync(`./src/commands`).filter(file => file.endsWith(`.js`) && !file.startsWith(`_`));
+        commandFiles.forEach(file => {
+	        const command = require(`../commands/${file}`);
+	        client.commands.set(command.name, command);
+	        log(`Loaded command ${command.name}`, `yellow`);
+        });
+    },
+
+    /* Run all methods */
+    async loadAll(client) {
+        await this.loadVariables(client);
+        await this.loadEvents(client);
+        await this.loadCommands(client);
+    }
+}
