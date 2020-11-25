e46b59dd8787a8bba0efb383be44634c8b351049
BR88C <bcroethel88@gmail.com>
2020-11-25 14:56:18 -0500

    make needed commands incompatable with livestreams

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -14,6 +14,9 @@ module.exports = {
 		// Checks if the user is in the VC
         if(message.member.voice.channelID !== serverQueue.channel.id) return message.reply(`you need to be in the same voice channel as me to pause the music!`);
 
+		// Checks if the current song is a livestream
+		if(serverQueue.songs[0].livestrean) return message.reply(`this command does not support livestreams!`);
+
 		// Checks if the music isn't paused
 		if (serverQueue.playing) {
 			serverQueue.playing = false;
--- src/commands/replay.js
+++ src/commands/replay.js
@@ -16,6 +16,9 @@ module.exports = {
 		// Checks if the user is in the VC
         if(message.member.voice.channelID !== serverQueue.channel.id) return message.reply(`you need to be in the same voice channel as me to replay the current song!`);
 
+		// Checks if the current song is a livestream
+		if(serverQueue.songs[0].livestrean) return message.reply(`this command does not support livestreams!`);
+		
 		// Plays the song from the beginning
 		serverQueue.songs[0].startTime = 0;
     	serverQueue.songs[0].hidden = false;
