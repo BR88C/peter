d422b8b1852cea688b4cb2f5cd48f7b7faeee4cc
BR88C <bcroethel88@gmail.com>
2020-11-28 11:42:45 -0500

    Create status.js

---
+++ src/commands/status.js
@@ -0,0 +1,22 @@
+const Discord = require(`discord.js`);
+const log = require(`../modules/log.js`);
+
+module.exports = {
+	name: `status`,
+	description: `Sets the bot's status`,
+	args: true,
+	devOnly: true,
+	hide: true,
+	usage: `<statusType> <content>`,
+	async execute(client, message, args) {
+        const availableTypes = [`PLAYING`, `STREAMING`, `LISTENING`, `WATCHING`, `COMPETING`];
+
+        const statusType = args[0].toUpperCase();
+        if(!availableTypes.includes(statusType)) return message.channel.send(`Error: invalid status type`)
+
+        const content = args.slice(1).join(' ');
+
+        client.user.setActivity(content, { type: statusType });
+        return message.channel.send(`Status successfully updated.`)
+	},
+}
