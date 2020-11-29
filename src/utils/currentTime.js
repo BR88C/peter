b2d885085fcf7be008522aa90648d8d4d81fb8e2
BR88C <bcroethel88@gmail.com>
2020-11-29 13:52:17 -0500

    round output

--- src/utils/currentTime.js
+++ src/utils/currentTime.js
@@ -1,5 +1,5 @@
 /* Returns the current time in a song based on stream time, speed, and the starting time */
 
 module.exports = (serverQueue) => {
-    return (serverQueue.connection.dispatcher.streamTime / 1000) * (serverQueue.speed / 100) + serverQueue.songs[serverQueue.currentSong].startTime;
+    return Math.round((serverQueue.connection.dispatcher.streamTime / 1000) * (serverQueue.speed / 100) + serverQueue.songs[serverQueue.currentSong].startTime);
 }
