eb406e2ab067897c812a5d34ef2c9442b74275b1
BR88C <bcroethel88@gmail.com>
2020-11-30 14:23:53 -0500

    add color

--- src/commands/cointoss.js
+++ src/commands/cointoss.js
@@ -6,20 +6,20 @@ module.exports = {
 	description: `Flips a coin, returns heads or tails`,
 	aliases: [`coin`, `coinflip`],
 	async execute(client, message, args) {
-        	const value = Math.round(Math.random());
+		const value = Math.round(Math.random());
 
-        	if(value === 1) {
-            		let headsEmbed = new Discord.MessageEmbed()
-                	.setColor()
-                	.setTitle(`The coin landed on Heads!`);
+		if(value === 1) {
+			let headsEmbed = new Discord.MessageEmbed()
+				.setColor(0xd4d4d4)
+				.setTitle(`The coin landed on Heads!`);
 
-            		return message.channel.send(headsEmbed);
-        	} else {
-            		let tailsEmbed = new Discord.MessageEmbed()
-                		.setColor()
-                		.setTitle(`The coin landed on Tails!`);
+				return message.channel.send(headsEmbed);
+		} else {
+			let tailsEmbed = new Discord.MessageEmbed()
+				.setColor(0xd4d4d4)
+				.setTitle(`The coin landed on Tails!`);
 
-            		return message.channel.send(tailsEmbed);
-        	}
+			return message.channel.send(tailsEmbed);
+		}
 	},
 }
