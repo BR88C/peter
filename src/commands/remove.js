46d43d19987febe3bcde68d05fc904cb5711013c
BR88C <bcroethel88@gmail.com>
2020-11-29 20:30:17 -0500

    implement checkValueSpecified

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -1,5 +1,6 @@
 const Discord = require(`discord.js`);
 const log = require(`../modules/log.js`);
+const checkValueSpecified = require(`../utils/checkValueSpecified.js`);
 
 module.exports = {
 	name: `remove`,
@@ -17,21 +18,15 @@ module.exports = {
 		// Checks if the user is in the VC
         if(message.member.voice.channelID !== serverQueue.channel.id) return message.reply(`you need to be in the same voice channel as me to remove music from the queue!`);
         
-        // Set specified index
-        const specifiedIndex = parseInt(args[0]);
-
-        // Checks if the argument provided is an integer
-        if(isNaN(specifiedIndex)) return message.reply(`please specify an Integer!`);
+        // Checks to make sure the value specified is valid
+		const specifiedValue = checkValueSpecified(args[0], 1, serverQueue.songs.length, message);
+        if(specifiedValue === `invalid`) return;
         
         // Checks if the song trying to be removed is the current song
-        if(specifiedIndex === serverQueue.currentSong) return message.reply(`you can't remove the current song playing!`);
-
-        // Checks if the queue has a song tagged with the number specified
-        const queueLength = (serverQueue.songs).length;
-        if(specifiedIndex > queueLength || specifiedIndex < 1 ) return message.reply(`there isnt a song in the queue with that number!`);
+        if(specifiedValue - 1 === serverQueue.currentSong) return message.reply(`you can't remove the song currently playing!`);
 
         // Removes the specified song from the queue
-        const song = serverQueue.songs.splice(specifiedIndex - 1, 1);
+        const song = serverQueue.songs.splice(specifiedValue - 1, 1);
 
         let removeEmbed = new Discord.MessageEmbed()
             .setColor(0xff668a)
