71beddbd723d048ec3028f2e59aa732198addab7
BR88C <bcroethel88@gmail.com>
2020-11-30 13:23:33 -0500

    fix tabbing

--- src/utils/checkValueSpecified.js
+++ src/utils/checkValueSpecified.js
@@ -4,13 +4,16 @@ module.exports = (input, min, max, message, offString) => {
 	let specifiedValue = input;
 	if(offString && specifiedValue.toLowerCase() === offString) specifiedValue = 0;
 	specifiedValue = parseInt(specifiedValue);
+
 	if(isNaN(specifiedValue)) {
-        message.channel.send(`Error: Invalid value! Please specify an Integer.`);
-        return `invalid`;
-    }
-    if(specifiedValue > max || specifiedValue < min) {
-        message.channel.send(`Error: Invalid value! Please specify a value between ${min} and ${max}.`);
-        return `invalid`;
-    }
-    return specifiedValue;
+		message.channel.send(`Error: Invalid value! Please specify an Integer.`);
+		return `invalid`;
+    	}
+
+    	if(specifiedValue > max || specifiedValue < min) {
+        	message.channel.send(`Error: Invalid value! Please specify a value between ${min} and ${max}.`);
+        	return `invalid`;
+    	}
+
+    	return specifiedValue;
 }
