2886295ad24cbae938e304ce01f89586c5458ec7
BR88C <bcroethel88@gmail.com>
2020-12-02 11:05:52 -0500

    simpligy setting next song's start time and hidden value

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -15,11 +15,9 @@ module.exports = {
         // Checks if the user is in the VC
         if (message.member.voice.channelID !== serverQueue.channel.id) return message.reply(`you need to be in the same voice channel as me to skip music!`);
 
-        // Sets the starTime to 0 and hidden to false for all songs
-        serverQueue.songs.forEach(song => {
-            song.startTime = 0;
-            song.hidden = false
-        });
+        // Sets the starTime to 0 and hidden to false for next song
+        serverQueue.songs[serverQueue.currentSong + 1].startTime = 0;
+        serverQueue.songs[serverQueue.currentSong + 1].hidden = false;
 
         // Skips to the next song
         serverQueue.connection.dispatcher.end();
