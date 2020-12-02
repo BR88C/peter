768e477c49f829d3cdc407188dbbdea501f509f7
BR88C <bcroethel88@gmail.com>
2020-12-02 11:06:17 -0500

    simplify async functions

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,8 +3,8 @@
 const Discord = require(`discord.js`);
 const log = require(`../modules/log.js`);
 
-module.exports = (client) => {
-    client.user.setPresence({
+module.exports = async (client) => {
+    await client.user.setPresence({
         activity: {
             name: `a kid's guide to the Internet`,
             type: 'WATCHING'
@@ -16,12 +16,11 @@ module.exports = (client) => {
     log(`Using prefix ${client.config.prefix}`, `green`);
     log(`Running version ${client.pjson.version}`, `green`);
 
-    async function readyStats () {
-        const guilds = await client.guilds.cache.size;
-        const channels = await client.channels.cache.size;
-        const users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
-        log(`Ready! Serving ${guilds} servers, ${channels} channels and ${users} users!`, `green`);
-        log(`\n--------------------------------------------------------------------------\n`, `white`);
-    }
-    readyStats();
+    const guilds = await client.guilds.cache.size;
+    const channels = await client.channels.cache.size;
+    const users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+
+    log(`Ready! Serving ${guilds} servers, ${channels} channels and ${users} users!`, `green`);
+    log(`\n--------------------------------------------------------------------------\n`, `white`);
+
 }
--- src/modules/end.js
+++ src/modules/end.js
@@ -2,23 +2,19 @@
 
 const log = require(`./log.js`);
 
-module.exports = (client, setDND, message) => {
-    async function exit () {
-        if (message) log(`\n${message}`, `red`);
-        log(`\nStopped. Bot Offline.`, `red`);
-        log(``, `white`);
-        process.exit();
-    }
-
+module.exports = async (client, setDND, message) => {
     if (setDND) {
-        client.user.setPresence({
+        await client.user.setPresence({
             activity: {
                 name: `Restarting Bot`,
                 type: 'PLAYING'
             },
             status: 'dnd'
-        }).then(() => exit()).catch(error => log(error, `red`));
-    } else {
-        exit();
+        }).catch(error => log(error, `red`));
     }
+
+    if (message) log(`\n${message}`, `red`);
+    log(`\nStopped. Bot Offline.`, `red`);
+    log(``, `white`);
+    process.exit();
 }
