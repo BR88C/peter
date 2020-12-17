c22ea16c5f26da1061e80201a42a4bb0c46fe2ab
BR88C <bcroethel88@gmail.com>
2020-12-17 09:42:30 -0500

    Create shuffleArray.js

---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,17 @@
+/* Shuffles a provided array */
+
+module.exports = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
