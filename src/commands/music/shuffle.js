daff48e771c4f5751ac202a5f00ca11925444391
BR88C <bcroethel88@gmail.com>
2020-12-17 09:42:39 -0500

    add shuffle command

---
+++ src/commands/music/shuffle.js
@@ -0,0 +1,35 @@
+const Discord = require(`discord.js-light`);
+const log = require(`../../modules/log.js`);
+const shuffleArray = require(`../../utils/shuffleArray.js`);
+
+module.exports = {
+    name: `shuffle`,
+    description: `Shuffles the queue`,
+    guildOnly: true,
+    async execute (client, message, args) {
+        // If the queue is empty reply with an error
+        const serverQueue = message.client.queue.get(message.guild.id);
+        if (!serverQueue || !serverQueue.songs[0]) return message.reply(`I can't shuffle the queue if there are no songs in the queue!`);
+
+        // Checks if the user is in the VC
+        if (message.member.voice.channelID !== serverQueue.channel.id) return message.reply(`you need to be in the same voice channel as me to shuffle the queue!`);
+
+        // Sets the starTime to 0 and hidden to false for all songs
+        serverQueue.songs.forEach(song => {
+            song.startTime = 0;
+            song.hidden = false
+        });
+
+        // Skips to the specified song
+        serverQueue.currentSong = 0;
+        if (serverQueue.loop !== `single`) serverQueue.currentSong--;
+        serverQueue.songs = shuffleArray(serverQueue.songs);
+        serverQueue.connection.dispatcher.end();
+
+        let shuffleEmbed = new Discord.MessageEmbed()
+            .setColor(0x9cd6ff)
+            .setTitle(`🔀  Shuffled the queue`);
+
+        return message.channel.send(shuffleEmbed);
+    },
+}
