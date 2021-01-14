9bb202ec00d4ae82f50ce17968063567ec643856
BR88C <bcroethel88@gmail.com>
2021-01-13 21:53:25 -0500

    add random presence

---
+++ src/config/presence.js
@@ -0,0 +1,31 @@
+const presence = [{
+        activity: {
+            name: `a kid's guide to the Internet`,
+            type: `WATCHING`
+        },
+        status: `online`
+    },
+    {
+        activity: {
+            name: `sick jams`,
+            type: `LISTENING`
+        },
+        status: `online`
+    },
+    {
+        activity: {
+            name: `sudo help`,
+            type: `PLAYING`
+        },
+        status: `online`
+    },
+    {
+        activity: {
+            name: `on the internet`,
+            type: `PLAYING`
+        },
+        status: `online`
+    }
+];
+
+module.exports = presence;
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,25 +1,23 @@
 /* Once Ready, set status and show ready info */
 
 const Discord = require(`discord.js-light`);
+const presence = require(`../config/presence.js`);
 const log = require(`../modules/log.js`);
+const shuffleArray = require(`../utils/shuffleArray.js`)
 
 module.exports = async (client) => {
-    await client.user.setPresence({
-        activity: {
-            name: `a kid's guide to the Internet`,
-            type: 'WATCHING'
-        },
-        status: 'online'
-    });
+    const guilds = await client.guilds.cache.size;
+    const users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+
+    const setPresence = async () => await client.user.setPresence(shuffleArray(presence)[0]);
+
+    setPresence();
 
     log(`\n--------------------------------------------------------------------------\n`, `white`);
     log(`Using prefix ${client.config.prefix}`, `green`);
     log(`Running version ${client.pjson.version}`, `green`);
-
-    const guilds = await client.guilds.cache.size;
-    const users = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
-
     log(`Ready! Serving ${users} users in ${guilds} guilds!`, `green`);
     log(`\n--------------------------------------------------------------------------\n`, `white`);
 
+    setTimeout(() => setPresence(), 6e5);
 }
