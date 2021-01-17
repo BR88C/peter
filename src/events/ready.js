46167ac904868b3567da6f3b3a30b2ad8b7f78a0
BR88C <bcroethel88@gmail.com>
2021-01-17 17:35:13 -0500

    change color

--- src/events/ready.js
+++ src/events/ready.js
@@ -12,11 +12,11 @@ module.exports = async (client) => {
 
     setPresence();
 
-    log(`\n**************************************************************************\n`, `blue`);
+    log(`\n**************************************************************************\n`, `white`);
     log(`Using prefix ${client.config.prefix}`, `green`);
     log(`Running version ${client.pjson.version}`, `green`);
     log(`Ready! Serving ${guilds} guilds!`, `green`);
-    log(`\n**************************************************************************\n`, `blue`);
+    log(`\n**************************************************************************\n`, `white`);
 
     setTimeout(() => setPresence(), 6e5);
 }
