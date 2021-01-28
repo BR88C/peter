6c8dd1bdf22091abc2287dc1044f147814ec9704
BR88C <bcroethel88@gmail.com>
2021-01-27 20:31:05 -0500

    fix

--- src/modules/requestHeaders.js
+++ src/modules/requestHeaders.js
@@ -5,23 +5,6 @@ const log = require(`./log.js`);
 const randomInt = require(`../utils/randomInt.js`);
 
 const requestHeaders = {
-    getHeaders: () => {
-        if (!this.checkHeaders()) return;
-
-        let cookies = JSON.parse(process.env.COOKIES);
-        cookies = cookies[randomInt(0, cookies.length - 1)];
-
-        let identityToken = JSON.parse(process.env.YOUTUBE_IDENTITY_TOKENS);
-        identityToken = identityToken[randomInt(0, identityToken.length - 1)];
-
-        return {
-            headers: {
-                cookie: cookies,
-                'x-youtube-identity-token': identityToken
-            }
-        };
-    },
-
     checkHeaders: () => {
         if (!process.env.COOKIES) {
             log(`WARNING: Cookies request header is undefined.`, `yellow`)
@@ -52,6 +35,23 @@ const requestHeaders = {
         }
 
         return true;
+    },
+
+    getHeaders: () => {
+        if (!requestHeaders.checkHeaders()) return;
+
+        let cookies = JSON.parse(process.env.COOKIES);
+        cookies = cookies[randomInt(0, cookies.length - 1)];
+
+        let identityToken = JSON.parse(process.env.YOUTUBE_IDENTITY_TOKENS);
+        identityToken = identityToken[randomInt(0, identityToken.length - 1)];
+
+        return {
+            headers: {
+                cookie: cookies,
+                'x-youtube-identity-token': identityToken
+            }
+        };
     },
 
     setDefaultHeaders: () => {
