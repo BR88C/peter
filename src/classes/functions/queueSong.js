86846a4754936791d9d025c0f10fb715db17bacd
BR88C <bcroethel88@gmail.com>
2021-03-18 11:56:34 -0400

    remove catching errors on removeall

--- src/classes/functions/queueSong.js
+++ src/classes/functions/queueSong.js
@@ -89,7 +89,7 @@ const queueSong = async (song, message, hidden, serverQueue) => {
                 msg.delete().catch((error) => log(error, `red`));
             }).catch((error) => {
                 log(error, `red`);
-                msg.reactions.removeAll().catch((error) => log(error, `red`));
+                msg.reactions.removeAll();
             });
         });
     }
