7ff29ca0db85f52390bc9a90f7647a44be5aa8a1
BR88C <bcroethel88@gmail.com>
2021-03-18 21:19:12 -0400

    fix currenttime throwing an error if dispatcher is undefined

--- src/utils/currentTime.js
+++ src/utils/currentTime.js
@@ -4,6 +4,6 @@
  * @param {Object} serverQueue Server queue object.
  * @returns {number} Current time.
  */
-const currentTime = (serverQueue) => Math.round((serverQueue.connection.dispatcher.streamTime / 1000) * (serverQueue.effects.speed / 100) + serverQueue.songs[serverQueue.currentSong].startTime);
+const currentTime = (serverQueue) => serverQueue.connection.dispatcher ? Math.round((serverQueue.connection.dispatcher.streamTime / 1000) * (serverQueue.effects.speed / 100) + serverQueue.songs[serverQueue.currentSong].startTime) : 0;
 
 module.exports = currentTime;
