83bae84ae0f2c73cbab0f6a40beb0f4d102dfe82
BR88C <bcroethel88@gmail.com>
2021-04-07 11:56:13 -0400

    remove moderation commands

--- src/config/cmddirectories/moderation.js
+++
@@ -1,9 +0,0 @@
-const moderationDir = {
-    name: `moderation`,
-    fullName: `Moderation`,
-    aliases: [],
-    description: `Commands with moderation features`,
-    ignore: false
-};
-
-module.exports = moderationDir;
