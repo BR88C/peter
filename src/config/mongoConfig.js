051925d3764babfae12cd5f09f22b1a7662e8fb6
BR88C <bcroethel88@gmail.com>
2021-04-14 10:53:51 -0400

    comments

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -1,7 +1,15 @@
+/**
+ * Configuration for Mongo Database.
+ */
 const mongoConfig = {
+    // The URL to connect to
     url: `mongodb://localhost:27017/`,
+
+    // The DB name (This and the URL is combined when connecting to the DB)
     dbName: `peter`,
-    collectionName: `guildconfigs`
+
+    // Models for DB entries
+    models: {}
 };
 
 module.exports = mongoConfig;
