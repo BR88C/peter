97f81dd8d34dc1eae05a91b420127fda2ebd1bcb
BR88C <bcroethel88@gmail.com>
2021-05-08 10:18:44 -0400

    remove createTimestamp

--- src/classes/Queue.js
+++ src/classes/Queue.js
@@ -159,23 +159,6 @@ class Queue {
 
         return activeEffects;
     }
-
-    /**
-     * Generates a timestamp based on the time specified in milliseconds.
-     * @param {number} time Time in milliseconds.
-     * @returns {String} Timestamp string.
-     */
-    createTimestamp (time) {
-        const hours = Math.floor(time / 1e3 / 60 / 60);
-        const minutes = Math.floor(time / 1e3 / 60) - (hours * 60);
-        const seconds = (time / 1e3) % 60;
-    
-        let formattedTime;
-        if (hours > 0) formattedTime = `${hours.toString()}:${minutes.toString().padStart(2, `0`)}:${seconds.toString().padStart(2, `0`)}`;
-        else formattedTime = `${minutes.toString()}:${seconds.toString().padStart(2, `0`)}`;
-
-        return formattedTime;
-    }
 }
 
 module.exports = Queue;
