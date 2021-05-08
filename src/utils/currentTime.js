2b7585777f88eabf0769d78f2ceb8d3f7c2a4166
BR88C <bcroethel88@gmail.com>
2021-05-08 09:56:16 -0400

    Update currentTime.js

--- src/utils/currentTime.js
+++ src/utils/currentTime.js
@@ -1,9 +1,8 @@
 /**
  * Returns the current time in a song based on stream time, speed, and the starting time.
- *
  * @param {Object} serverQueue Server queue object.
- * @returns {number} Current time in seconds.
+ * @returns {number} Current time in milliseconds.
  */
-const currentTime = (serverQueue) => serverQueue.connection.dispatcher ? (Math.round((serverQueue.connection.dispatcher.streamTime / 1000) * (serverQueue.effects.speed / 100)) + serverQueue.songs[serverQueue.currentSong].startTime) : 0;
+const currentTime = (serverQueue) => Math.round(((Date.now() - serverQueue.songPlayingSince) * (serverQueue.effects.speed / 100)) + serverQueue.songs[serverQueue.currentSong].startTime);
 
 module.exports = currentTime;
