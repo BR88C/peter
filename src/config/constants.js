a5402966defdf7cfb565d6c70e499d38f61fcb6e
BR88C <bcroethel88@gmail.com>
2021-05-20 14:19:02 -0400

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Constants for the bot.
+ * @type {Object}
+ * @property {string} INVITE_LINK The bot's invite link.
+ * @property {string} VOTE_LINK The bot's vote link.
+ * @property {string} WEBSITE The bot's website.
+ */
+const constants = {
+    INVITE_LINK: `https://discord.com/oauth2/authorize?client_id=744694015630245949&scope=bot%20applications.commands&permissions=3525696`,
+    VOTE_LINK: `https://top.gg/bot/744694015630245949/vote`,
+    WEBSITE: `https://peter.badfirmware.com`
+};
+
+module.exports = constants;
