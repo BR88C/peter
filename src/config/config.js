fb5ff338034a93fd28b6aa51ce515af9e6385d15
BR88C <bcroethel88@gmail.com>
2021-05-20 14:18:59 -0400

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+/**
+ * The bot's main config.
+ * @type {Object}
+ * @property {Object} prefix The bot's prefixes.
+ * @property {string} prefix.dev The prefix to use when NODE_ENV is "dev".
+ * @property {string} prefix.prod The prefix to use when NODE_ENV is "prod".
+ * @property {string} developerPrefix The prefix to be used when attempting to run dev only commands.
+ * @property {string} emojiGuildID The ID of the guild that contains all emoji's the bot uses.
+ * @property {Object.<string, string>} emojiIDs IDs of emojis to use.
+ */
+const config = {
+    prefix: {
+        dev: `%`,
+        prod: `!`
+    },
+    developerPrefix: `sudo`,
+    emojiGuildID: `844990450763169792`,
+    emojiIDs: {}
+}
+
+module.exports = config;
