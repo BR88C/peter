fb62fcce4c048c036215cc842e339af0c92e6330
BR88C <bcroethel88@gmail.com>
2021-05-20 22:15:03 -0400

    Update botinfo.js

--- src/commands/botinfo/botinfo.js
+++ src/commands/botinfo/botinfo.js
@@ -14,13 +14,12 @@ module.exports = {
             .title(`Bot Information`)
             .field(`**Tag**`, `${ctx.worker.user.username}#${ctx.worker.user.discriminator}`, true)
             .field(`**Number of Commands**`, ctx.worker.commands.commands.size, true)
-            .field(`**Prefix**`, `\`${ctx.prefix}\``, true)
+            .field(`**Version**`, pjson.version, true)
             .field(`**Developer${config.devs.IDs.length > 1 ? `s` : ``}**`, config.devs.tags.join(`, `), true)
             .field(`**Ping**`, `\`${ctx.worker.shards.find((shard) => shard.worker.guilds.has(ctx.message.guild_id)).ping} ms\``, true)
             .field(`**Uptime**`, timestamp(stats[ctx.worker.comms.id].cluster.uptime * 1e3), true)
             .field(`**Support Server**`, constants.SUPPORT_SERVER, true)
             .field(`**Website**`, constants.WEBSITE, true)
-            .field(`**Version**`, pjson.version, true)
             .send();
     }
 };
