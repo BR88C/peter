a21e39a00b927cb53798b4e9b2837ecdbeb5d29e
BR88C <bcroethel88@gmail.com>
2021-05-20 21:27:22 -0400

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,14 +1,18 @@
 /**
  * Constants for the bot.
- * @type {Object}
  * @property {string} INVITE_LINK The bot's invite link.
+ * @property {string} SUPPORT_SERVER The invite link to the bot's support server.
  * @property {string} VOTE_LINK The bot's vote link.
  * @property {string} WEBSITE The bot's website.
+ * @property {number} BOT_INFO_COLOR The color to use in the botinfo embed.
  */
 const constants = {
     INVITE_LINK: `https://discord.com/oauth2/authorize?client_id=744694015630245949&scope=bot%20applications.commands&permissions=3525696`,
+    SUPPORT_SERVER: `https://discord.gg/E2JsYPPJYN`,
     VOTE_LINK: `https://top.gg/bot/744694015630245949/vote`,
-    WEBSITE: `https://peter.badfirmware.com`
+    WEBSITE: `https://peter.badfirmware.com`,
+
+    BOT_INFO_COLOR: 0xFFD87D
 };
 
 module.exports = constants;
