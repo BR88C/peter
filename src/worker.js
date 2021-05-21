14a0b0746e06cf088e62c25b8adeeee875e3f814
BR88C <bcroethel88@gmail.com>
2021-05-20 22:36:35 -0400

    add middleware

--- src/worker.js
+++ src/worker.js
@@ -15,3 +15,9 @@ worker.log(`Using prefix ${config.prefix[process.env.NODE_ENV]}`);
 // Push all commands to the worker.
 for (const dir of fs.readdirSync(`./src/commands`).filter((file) => fs.statSync(`${`./src/commands` + `/`}${file}`).isDirectory())) worker.commands.load(path.resolve(__dirname, `./commands/${dir}`));
 worker.log(`Loaded ${worker.commands.commands.size} commands`);
+
+// Create middleware.
+worker.commands.middleware((ctx) => {
+    worker.log(`Command Request | Command: ${ctx.command.command} | User: ${ctx.message.author.username}#${ctx.message.author.discriminator} | Guild Name: ${ctx.worker.guilds.get(ctx.message.guild_id).name} | Guild ID: ${ctx.message.guild_id}`)
+    return true;
+});
