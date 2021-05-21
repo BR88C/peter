b7b480240ce9a5de0fe69570b4662c6d2ed81355
BR88C <bcroethel88@gmail.com>
2021-05-20 22:11:04 -0400

    Create ping.js

---
+++ src/commands/botinfo/ping.js
@@ -0,0 +1,12 @@
+const constants = require(`../../config/constants.js`);
+
+module.exports = {
+    command: `ping`,
+    exec: (ctx) => {
+        ctx.embed
+            .color(constants.PING_EMBED_COLOR)
+            .title(`Pong!`)
+            .description(`\`\`\`js\n${ctx.worker.shards.find((shard) => shard.worker.guilds.has(ctx.message.guild_id)).ping} ms\n\`\`\``)
+            .send();
+    }
+};
