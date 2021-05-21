25bdc418cf7e2d1b910809862e1ba8baee6090f8
BR88C <bcroethel88@gmail.com>
2021-05-20 22:36:24 -0400

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,13 @@
  * @property {Object} devs Config for the bot's devs.
  * @property {string[]} devs.IDs An array of dev IDs. Their indexes should match with devs.tags.
  * @property {string[]} devs.tags An array of dev tags. Their indexes should match with devs.IDs.
+ *
  * @property {string} emojiGuildID The ID of the guild that contains all emoji's the bot uses.
  * @property {Object.<string, string>} emojiIDs IDs of emojis to use.
+ *
+ * @property {Object} cache Cache options to be used when creating master.
+ * @property {Object} cacheControl Cache control options to be used when creating master.
+ *
  * @property {{ dev: string, prod: string }} prefix The bot's prefixes.
  * @property {{ dev: number | string, prod: number | string }} shards The total amount of shards to spawn.
  * @property {{ dev: number, prod: number }} shardsPerCluster The amount of shards to spawn per cluster.
@@ -21,6 +26,18 @@ const config = {
     emojiGuildID: `844990450763169792`,
     emojiIDs: {},
 
+    cache: {
+        channels: false,
+        guilds: true,
+        members: false,
+        messages: false,
+        roles: false,
+        self: true,
+        users: false,
+        voiceStates: true
+    },
+    cacheControl: { guilds: [`id`, `owner_id`, `member_count`, `name`] },
+
     prefix: {
         dev: `;`,
         prod: `!`
@@ -34,7 +51,7 @@ const config = {
         prod: 1
     },
     statsCheckupInterval: {
-        dev: 1e4,
+        dev: 3e4,
         prod: 12e4
     }
 };
