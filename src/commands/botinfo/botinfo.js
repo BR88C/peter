5e38d029e9ae7b9e32e7b6949f7431fefe4711db
BR88C <bcroethel88@gmail.com>
2021-05-20 22:11:12 -0400

    Update botinfo.js

--- src/commands/botinfo/botinfo.js
+++ src/commands/botinfo/botinfo.js
@@ -9,14 +9,14 @@ module.exports = {
     exec: async (ctx) => {
         const stats = await ctx.worker.comms.getStats();
         ctx.embed
-            .color(constants.BOT_INFO_COLOR)
+            .color(constants.BOT_INFO_EMBED_COLOR)
             .thumbnail(`https://cdn.discordapp.com/avatars/${ctx.worker.user.id}/${ctx.worker.user.avatar}.png`)
             .title(`Bot Information`)
             .field(`**Tag**`, `${ctx.worker.user.username}#${ctx.worker.user.discriminator}`, true)
             .field(`**Number of Commands**`, ctx.worker.commands.commands.size, true)
             .field(`**Prefix**`, `\`${ctx.prefix}\``, true)
             .field(`**Developer${config.devs.IDs.length > 1 ? `s` : ``}**`, config.devs.tags.join(`, `), true)
-            .field(`**Ping**`, `${ctx.worker.shards.find((shard) => shard.worker.guilds.has(ctx.message.guild_id)).ping}ms`, true)
+            .field(`**Ping**`, `\`${ctx.worker.shards.find((shard) => shard.worker.guilds.has(ctx.message.guild_id)).ping} ms\``, true)
             .field(`**Uptime**`, timestamp(stats[ctx.worker.comms.id].cluster.uptime * 1e3), true)
             .field(`**Support Server**`, constants.SUPPORT_SERVER, true)
             .field(`**Website**`, constants.WEBSITE, true)
