0ec18d31138819e510a8c3aa38eec5cdd0fc8174
BR88C <bcroethel88@gmail.com>
2021-05-20 21:47:43 -0400

    add grabbing uptime

--- src/commands/botinfo/botinfo.js
+++ src/commands/botinfo/botinfo.js
@@ -1,11 +1,13 @@
 const config = require(`../../config/config.js`);
 const constants = require(`../../config/constants.js`);
 const pjson = require(`../../../package.json`);
+const timestamp = require(`../../utils/timestamp.js`);
 
 module.exports = {
     command: `botinfo`,
     aliases: [`about`],
-    exec: (ctx) => {
+    exec: async (ctx) => {
+        const stats = await ctx.worker.comms.getStats();
         ctx.embed
             .color(constants.BOT_INFO_COLOR)
             .thumbnail(`https://cdn.discordapp.com/avatars/${ctx.worker.user.id}/${ctx.worker.user.avatar}.png`)
@@ -15,7 +17,7 @@ module.exports = {
             .field(`**Prefix**`, `\`${ctx.prefix}\``, true)
             .field(`**Developer${config.devs.IDs.length > 1 ? `s` : ``}**`, config.devs.tags.join(`, `), true)
             .field(`**Ping**`, `${ctx.worker.shards.find((shard) => shard.worker.guilds.has(ctx.message.guild_id)).ping}ms`, true)
-            .field(`**Uptime**`, `undefine`, true)
+            .field(`**Uptime**`, timestamp(stats[ctx.worker.comms.id].cluster.uptime * 1e3), true)
             .field(`**Support Server**`, constants.SUPPORT_SERVER, true)
             .field(`**Website**`, constants.WEBSITE, true)
             .field(`**Version**`, pjson.version, true)
