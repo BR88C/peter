372a1906ac27ddb02d820f378d5d4e9399d74458
BR88C <bcroethel88@gmail.com>
2021-05-24 15:14:00 -0400

    Update Song.js

--- src/structures/Song.js
+++ src/structures/Song.js
@@ -1,4 +1,5 @@
 const cleanseMarkdown = require(`../utils/cleanseMarkdown.js`);
+const Format = require(`./Format.js`);
 
 /**
  * Song classs - Used for creating song objects to be added to the queue.
@@ -43,51 +44,19 @@ class Song {
         this.livestream = songInfo.videoDetails.isLiveContent;
 
         /**
-         * If the "now playing" embed should be hidden when playing the song.
-         * @type {boolean}
-         */
-        this.hidden = false;
-
-        /**
          * Available formats.
          * @type {object[]}
          */
-        this.formats = songInfo.formats;
+        this.formats = {
+            opus: new Format(songInfo.formats, this.videoLength, true, this.livestream),
+            nonOpus: new Format(songInfo.formats, this.videoLength, false, this.livestream)
+        }
 
         /**
          * The tag of the person who requested the song.
          * @type {string}
          */
         this.requestedBy = messageAuthorTag;
-    }
-
-    /**
-     * Get the song's opus format, if it supports opus encoding.
-     * @returns {string | undefined}
-     */
-    get opusFormat () {
-        const formats = (this.livestream ? this.formats.filter((option) => option.isHLS) : this.formats)?.filter((option) =>
-            this.videoLength !== 0 &&
-            option.audioBitrate &&
-            option.codecs === `opus` &&
-            option.audioCodec === `opus` &&
-            option.container === `webm` &&
-            parseInt(option.audioSampleRate) === 48e3
-        );
-        if (formats?.length !== 0) return formats.sort((a, b) => b.audioBitrate - a.audioBitrate)[0].itag.toString();
-        else return undefined;
-    }
-
-    /**
-     * Get the song's ffmpeg format, if it supports ffmpeg encoding.
-     * @returns {string | undefined}
-     */
-    get ffmpegFormat () {
-        const formats = (this.livestream ? this.formats.filter((option) => option.isHLS) : this.formats)?.filter((option) => option.audioBitrate);
-        const audioOnlyFormats = formats?.filter((option) => option.hasAudio && !option.hasVideo);
-        if (audioOnlyFormats?.length !== 0) return audioOnlyFormats.sort((a, b) => b.audioBitrate - a.audioBitrate)[0].itag.toString();
-        else if (formats?.length !== 0) return formats.sort((a, b) => b.audioBitrate - a.audioBitrate)[0].itag.toString();
-        else return undefined;
     }
 }
 
