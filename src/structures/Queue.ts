0bf53917f4176d10b23663f62ece4e42ce715361
BR88C <bcroethel88@gmail.com>
2021-05-27 16:01:04 -0400

    add remaining properties to the queue class

--- src/structures/Queue.ts
+++ src/structures/Queue.ts
@@ -1,8 +1,13 @@
+import { PlaybackActivity } from './PlaybackActivity';
 import { Song } from './Song';
 
 // Import modules.
 import { Snowflake } from 'discord-api-types';
 
+/**
+ * Queue class - Manages all music / audio for a guild.
+ * @class
+ */
 export class Queue {
     /**
      * The ID of the text channel the queue is bound to.
@@ -16,14 +21,29 @@ export class Queue {
      * The ID of the guild the queue is bound to.
      */
     public guildID: Snowflake
+
     /**
      * The queue's songs.
      */
     public songs: Song[]
     /**
      * The current song playing, represented as an index of Queue#songs.
+     * This value is set to -1 if no songs are playing.
      */
     public playing: number
+    /**
+     * If the queue is paused.
+     */
+    public paused: boolean
+    /**
+     * If the music should be looped.
+     */
+    public loop: `off` | `queue` | `single`
+    /**
+     * If the queue should be played 24/7.
+     */
+    public twentyFourSeven: boolean
+
     /**
      * The queue's effects.
      */
@@ -92,10 +112,18 @@ export class Queue {
     }
 
     /**
+     * Playback data.
+     * Used for determining the progress through a song.
+     * This is undefined if no song is playing.
+     */
+    private readonly playbackActivity: PlaybackActivity | undefined
+
+    /**
      * Creates a Queue.
      * @param textID The text channel ID to bind to.
      * @param voiceID The voice channel ID to bind to.
      * @param guildID The guild the queue is bound to.
+     * @constructor
      */
     constructor (textID: Snowflake, voiceID: Snowflake, guildID: Snowflake) {
         this.textID = textID;
@@ -103,7 +131,10 @@ export class Queue {
         this.guildID = guildID;
 
         this.songs = [];
-        this.playing = 0;
+        this.playing = -1;
+        this.paused = false;
+        this.loop = `off`;
+        this.twentyFourSeven = false;
 
         this.effects = {
             bass: 0,
@@ -117,5 +148,16 @@ export class Queue {
             vibrato: 0,
             volume: 100
         };
+
+        this.playbackActivity = undefined;
+    }
+
+    /**
+     * Changes the speed of the queue, and adds a segment to the queue's playback activity.
+     * @param newSpeed The new speed to set the queue to.
+     */
+    public changeSpeed (newSpeed: number): void {
+        this.effects.speed = newSpeed;
+        if (this.playbackActivity) this.playbackActivity.addSegment(newSpeed);
     }
 }
