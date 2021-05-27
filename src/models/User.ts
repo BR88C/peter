d73da4fe06e684b11f076c7c6edec19a030314ce
BR88C <bcroethel88@gmail.com>
2021-05-27 10:14:00 -0400

    Create User.ts

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+    /**
+     * The User's ID.
+     */
+    id: String,
+    /**
+     * If the User is a premium User.
+     */
+    premium: Boolean,
+    /**
+     * The User's search mode.
+     * 0 = The first result of a search query is used.
+     * 1 = The user is shown an empheral with the firt 5 results.
+     * 2 = The user is shown an empheral with the first 10 results.
+     */
+    searchMode: Number
+});
+
+export const User = model(`User`, UserSchema);
