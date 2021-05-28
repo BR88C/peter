8c4dd3c51a113c7d43886afbea181db94d936d68
BR88C <bcroethel88@gmail.com>
2021-05-27 22:24:15 -0400

    Create play.ts

---
+++ src/commands/music/play.ts
@@ -0,0 +1,21 @@
+import { Queue } from '../../structures/Queue';
+
+// Import modules.
+import { CommandOptions } from 'discord-rose';
+
+export default {
+    command: `play`,
+    interaction: {
+        name: `play`,
+        description: `Plays a specified song.`
+    },
+    exec: async (ctx) => {
+        let queue: Queue;
+        if (ctx.channel && ctx.guild) queue = new Queue(ctx.channel.id, `738892661150187544`, ctx.guild.id, ctx.worker);
+        else return await ctx.error(`An unknown error occured when trying to connect to the voice channel.`);
+
+        queue.createConnection().then(() => {
+            queue.playSong().then(async () => await ctx.send(`epic`)).catch(async (error) => await ctx.error(error));
+        }).catch(async (error) => await ctx.error(error));
+    }
+} as CommandOptions;
