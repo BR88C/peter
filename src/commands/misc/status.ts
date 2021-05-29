4035ad5dbd93bd7d72f27426919fca446f1ee75e
BR88C <bcroethel88@gmail.com>
2021-05-28 21:54:26 -0400

    Modify status error message

--- src/commands/misc/status.ts
+++ src/commands/misc/status.ts
@@ -7,7 +7,7 @@ import { CommandOptions } from 'discord-rose';
 export default {
     command: `status`,
     exec: async (ctx) => {
-        if (!Constants.PRESENCE_TYPES.includes(ctx.args[0])) void ctx.error(`Invalid status type`);
+        if (!Constants.PRESENCE_TYPES.includes(ctx.args[0])) void ctx.error(`Invalid status type.`);
         else {
             await ctx.worker.setStatus(ctx.args[0], ctx.args.slice(1).join(` `), `online` as PresenceUpdateStatus);
             ctx.embed
