a89566afe964fda8f493274aa45760bcfee0aa45
BR88C <bcroethel88@gmail.com>
2021-06-02 13:53:41 -0400

    Remove unneeded async function

--- src/commands/misc/cointoss.ts
+++ src/commands/misc/cointoss.ts
@@ -9,7 +9,7 @@ export default {
         name: `cointoss`,
         description: `Tosses a coin, and returns heads or tails.`
     },
-    exec: async (ctx) => {
+    exec: (ctx) => {
         ctx.embed
             .color(Constants.COIN_TOSS_EMBED_COLOR)
             .title(`The coin landed on ${Math.random() >= 0.5 ? `heads` : `tails`}!`)
--- src/commands/misc/status.ts
+++ src/commands/misc/status.ts
@@ -6,10 +6,10 @@ import { CommandOptions } from 'discord-rose';
 
 export default {
     command: `status`,
-    exec: async (ctx) => {
+    exec: (ctx) => {
         if (!Constants.PRESENCE_TYPES.includes(ctx.args[0])) void ctx.error(`Invalid status type.`);
         else {
-            await ctx.worker.setStatus(ctx.args[0], ctx.args.slice(1).join(` `), `online` as PresenceUpdateStatus);
+            ctx.worker.setStatus(ctx.args[0], ctx.args.slice(1).join(` `), `online` as PresenceUpdateStatus);
             ctx.embed
                 .color(Constants.STATUS_EMBED_COLOR)
                 .title(`Updated status successfully`)
