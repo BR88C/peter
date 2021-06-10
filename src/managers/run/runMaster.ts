7cd46f65dabed624f69039fbf49b574de48e5b5a
BR88C <bcroethel88@gmail.com>
2021-06-10 11:16:07 -0400

    Remove files

--- src/managers/run/runMaster.ts
+++
@@ -1,6 +0,0 @@
-import { MasterManager } from '../MasterManager';
-
-/**
- * Starts a master manager process.
- */
-export default (): MasterManager => new MasterManager();
--- src/managers/run/runWorker.ts
+++
@@ -1,7 +0,0 @@
-import { WorkerManager } from '../WorkerManager';
-
-void new WorkerManager();
-
-declare module 'discord-rose/dist/typings/lib' { // eslint-disable-line quotes
-    type worker = WorkerManager
-};
