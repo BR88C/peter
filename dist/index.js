40f3527f68036c3605070a7fbb39fe150d810c88
BR88C <br88c@badfirmware.com>
2021-07-04 02:31:37 +0000

    build: TSC build for c083cd2950f334a876a32c3b8ef0457843fad5cf

--- dist/index.js
+++ dist/index.js
@@ -7,5 +7,7 @@ const Log_1 = require("./utils/Log");
 const runMaster_1 = __importDefault(require("./managers/run/runMaster"));
 const dotenv_1 = require("dotenv");
 dotenv_1.config();
+if (process.env.NODE_ENV !== `dev` && process.env.NODE_ENV !== `prod`)
+    throw new Error(`NODE_ENV is not properly set.`);
 Log_1.logHeader();
 runMaster_1.default();
