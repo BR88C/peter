5afb5df40e35ae2811527c8fcb8108c4dd353b6e
BR88C <bcroethel88@gmail.com>
2021-07-03 23:10:28 -0400

    Update Guild.ts

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -16,12 +16,6 @@ const GuildSchema: Schema = new Schema({
      * 2 = Non-DJs cannot do anything except view the queue and the current song playing.
      */
     DJMode: Number,
-    /**
-     * Song skip mode.
-     * 0 = Normal skip. If DJMode is 1, non-DJs can only voteskip. If DJMode is 2, non-DJs cannot skip.
-     * 1 = Voteskip is on always for non-DJs, even if DJMode is 0. DJs can still surpass voteskip.
-     */
-    skipMode: Number
 });
 
 export const Guild = model(`Guild`, GuildSchema);
