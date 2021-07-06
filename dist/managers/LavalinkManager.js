fe4d0087ab4ba3eaf039e1f1b88e864b23ce9981
BR88C <br88c@badfirmware.com>
2021-07-06 22:41:54 +0000

    build: TSC build for 51bd9c122e3df5400475b2a3ad9d8619fbdfcd8c

--- dist/managers/LavalinkManager.js
+++ dist/managers/LavalinkManager.js
@@ -15,11 +15,11 @@ class LavalinkManager extends erela_js_1.Manager {
         });
         this.worker = worker;
         this
-            .on(`nodeConnect`, (node) => this.worker.log(`\x1b[32mLavalink Node Connected | Node ID: ${node.options.identifier}`))
-            .on(`nodeError`, (node, error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: ${error.message} | Node ID: ${node.options.identifier}`))
-            .on(`playerCreate`, (player) => this.worker.log(`Created new Player | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`))
+            .on(`nodeConnect`, (node) => this.worker.log(`\x1b[32mLavalink Node Connected | Node: ${node.options.host}:${node.options.port}`))
+            .on(`nodeError`, (node, error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: ${error.message} | Node: ${node.options.host}:${node.options.port}`))
+            .on(`playerCreate`, (player) => this.worker.log(`Created new Player | Node: ${player.node.options.host}:${player.node.options.port} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`))
             .on(`trackStart`, (player, track) => {
-            this.worker.log(`\x1b[32mStarted Track | Track identifier: ${track.identifier} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`);
+            this.worker.log(`\x1b[32mStarted Track | Track identifier: ${track.identifier} | Node: ${player.node.options.host}:${player.node.options.port} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`);
             const trackStartEmbed = new discord_rose_1.Embed()
                 .color(Constants_1.Constants.STARTED_PLAYING_EMBED_COLOR)
                 .title(`Started playing: ${StringUtils_1.cleanseMarkdown(track.title)}`)
@@ -27,15 +27,15 @@ class LavalinkManager extends erela_js_1.Manager {
                 .description(`**Link:** https://youtu.be/${track.identifier}`)
                 .footer(`Requested by ${track.requester}`)
                 .timestamp();
-            this.worker.api.messages.send(player.textChannel, trackStartEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track start embed => ${error.message} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
+            this.worker.api.messages.send(player.textChannel, trackStartEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track start embed => ${error.message} | Node: ${player.node.options.host}:${player.node.options.port} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
         })
             .on(`trackError`, (player, track) => {
-            this.worker.log(`\x1b[31mLavalink Node Error | Error: Track error => Track identifier: ${track.identifier} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`);
+            this.worker.log(`\x1b[31mLavalink Node Error | Error: Track error => Track identifier: ${track.identifier} | Node: ${player.node.options.host}:${player.node.options.port} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`);
             const trackErrorEmbed = new discord_rose_1.Embed()
                 .color(Constants_1.Constants.ERROR_EMBED_COLOR)
                 .title(`Error`)
                 .description(`\`\`\`\nAn unknown track error occured.\n\`\`\`\n*If this doesn't seem right, please submit an issue in the support server:* ${Constants_1.Constants.SUPPORT_SERVER}`);
-            this.worker.api.messages.send(player.textChannel, trackErrorEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track error embed => ${error.message} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
+            this.worker.api.messages.send(player.textChannel, trackErrorEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track error embed => ${error.message} | Node: ${player.node.options.host}:${player.node.options.port} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
         });
         this.worker.on(`*`, (data) => this.updateVoiceState(data));
     }
