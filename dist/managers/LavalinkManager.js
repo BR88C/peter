f04847b8495a78747b5972dea6cbca80980e50ec
BR88C <br88c@badfirmware.com>
2021-07-06 21:43:18 +0000

    build: TSC build for 067f2893c0852848d814dff4f82687e5515ed4d8

--- dist/managers/LavalinkManager.js
+++ dist/managers/LavalinkManager.js
@@ -10,7 +10,7 @@ class LavalinkManager extends erela_js_1.Manager {
         super({
             nodes: nodes,
             send: (id, payload) => {
-                this.guildShard(id).ws._send(payload);
+                this.worker.guildShard(id).ws._send(payload);
             }
         });
         this.worker = worker;
@@ -23,8 +23,8 @@ class LavalinkManager extends erela_js_1.Manager {
             const trackStartEmbed = new discord_rose_1.Embed()
                 .color(Constants_1.Constants.STARTED_PLAYING_EMBED_COLOR)
                 .title(`Started playing: ${StringUtils_1.cleanseMarkdown(track.title)}`)
-                .description(`**Link:** ${track.identifier}`)
-                .image(`${track.displayThumbnail(`maxresdefault`)}`)
+                .thumbnail(`${track.displayThumbnail()}`)
+                .description(`**Link:** https://youtu.be/${track.identifier}`)
                 .footer(`Requested by ${track.requester}`)
                 .timestamp();
             this.worker.api.messages.send(player.textChannel, trackStartEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track start embed => ${error.message} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
@@ -37,8 +37,7 @@ class LavalinkManager extends erela_js_1.Manager {
                 .description(`\`\`\`\nAn unknown track error occured.\n\`\`\`\n*If this doesn't seem right, please submit an issue in the support server:* ${Constants_1.Constants.SUPPORT_SERVER}`);
             this.worker.api.messages.send(player.textChannel, trackErrorEmbed).catch((error) => this.worker.log(`\x1b[31mLavalink Node Error | Error: Unable to send track error embed => ${error.message} | Node ID: ${player.node.options.identifier} | Guild Name: ${this.worker.guilds.get(player.guild)?.name} | Guild ID: ${player.guild}`));
         });
-        this.worker.on(`VOICE_SERVER_UPDATE`, (data) => this.updateVoiceState(data));
-        this.worker.on(`VOICE_STATE_UPDATE`, (data) => this.updateVoiceState(data));
+        this.worker.on(`*`, (data) => this.updateVoiceState(data));
     }
 }
 exports.LavalinkManager = LavalinkManager;
