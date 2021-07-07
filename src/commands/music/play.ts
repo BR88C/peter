9b4372e25bc2fb33801dd02e6bbf6866ff79b1ff
BR88C <bcroethel88@gmail.com>
2021-07-06 21:33:05 -0400

    Update play.ts

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,7 +1,9 @@
+import { cleanseMarkdown } from '../../utils/StringUtils';
 import { Constants } from '../../config/Constants';
 
 // Import modules.
 import { CommandOptions } from 'discord-rose';
+import { Player, SearchResult } from 'erela.js';
 
 export default {
     command: `play`,
@@ -28,5 +30,38 @@ export default {
             .title(`Processing query...`)
             .send()
             .catch((error) => void ctx.error(error));
+
+        const search: SearchResult = await ctx.worker.lavalink.search(ctx.options.query, `${ctx.interaction.member.user.username}#${ctx.interaction.member.user.discriminator}`);
+        if (!search.tracks.length) return void ctx.error(`Unable to find any results based on the provided query.`);
+        console.log(search.tracks.length)
+
+        const player: Player = ctx.worker.lavalink.create({
+            guild: ctx.interaction.guild_id,
+            voiceChannel: foundVoiceState.channel_id,
+            textChannel: ctx.interaction.channel_id
+        });
+        player.connect();
+
+        if (!search.playlist) {
+            player.queue.add(search.tracks[0]);
+            await ctx.embed
+                .color(Constants.ADDED_TO_QUEUE_EMBED_COLOR)
+                .title(`Added "${cleanseMarkdown(search.tracks[0].title)}" to the queue`)
+                .footer(`Requested by ${search.tracks[0].requester}`)
+                .timestamp()
+                .send()
+                .catch((error) => void ctx.error(error));
+        } else {
+            for (const track of search.tracks) player.queue.add(track);
+            await ctx.embed
+                .color(Constants.ADDED_TO_QUEUE_EMBED_COLOR)
+                .title(`Successfully queued ${search.tracks.length} song${search.tracks.length > 1 ? `s` : ``}`)
+                .footer(`Requested by ${search.tracks[0].requester}`)
+                .timestamp()
+                .send()
+                .catch((error) => void ctx.error(error));
+        }
+
+        if (!player.playing && !player.paused) player.play().catch((error) => void ctx.error(error));
     }
 } as CommandOptions;
