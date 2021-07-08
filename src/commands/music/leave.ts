3d033846908747ce725930f9ac9dfa115b640486
BR88C <bcroethel88@gmail.com>
2021-07-08 12:26:57 -0400

    Create leave.ts

---
+++ src/commands/music/leave.ts
@@ -0,0 +1,25 @@
+import { Constants } from '../../config/Constants';
+import { ExtendedPlayer } from '../../managers/LavalinkManager';
+
+// Import modules.
+import { CommandOptions } from 'discord-rose';
+
+export default {
+    command: `leave`,
+    interaction: {
+        name: `leave`,
+        description: `Disconnect the bot and destroy the queue.`
+    },
+    exec: (ctx) => {
+        const player: ExtendedPlayer | undefined = ctx.worker.lavalink.players.get(ctx.interaction.guild_id) as any;
+        if (!player) return void ctx.error(`Unable to disconnect the bot; no music is playing.`);
+
+        player.destroy();
+
+        ctx.embed
+            .color(Constants.LEAVE_EMBED_COLOR)
+            .title(`:wave:  Left the VC`)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+} as CommandOptions;
