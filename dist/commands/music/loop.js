21a4478060885aea71ddbece92c4e32832f00742
BR88C <br88c@badfirmware.com>
2021-07-08 16:30:03 +0000

    build: TSC build for e58752051f70f0bcb8d48d6fa3bb6253e72e4cf7

--- dist/commands/music/loop.js
+++ dist/commands/music/loop.js
@@ -8,7 +8,7 @@ exports.default = {
         description: `Modify looping behavior.`,
         options: [
             {
-                type: 4,
+                type: 3,
                 name: `type`,
                 description: `The type of looping to use.`,
                 choices: [
