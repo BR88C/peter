b132b5760b8c0906390f9d77629bdf93d842130c
BR88C <bcroethel88@gmail.com>
2021-07-08 14:26:24 -0400

    Fix error message

--- src/commands/music/pause.ts
+++ src/commands/music/pause.ts
@@ -12,7 +12,7 @@ export default {
     },
     exec: (ctx) => {
         const player: ExtendedPlayer | undefined = ctx.worker.lavalink.players.get(ctx.interaction.guild_id) as any;
-        if (!player || !player.queue?.current) return void ctx.error(`Unable to disconnect the bot; no music is playing.`);
+        if (!player || !player.queue?.current) return void ctx.error(`Unable to pause the music; no music is playing.`);
 
         if (player.paused) return void ctx.error(`The music is already paused.`);
 
