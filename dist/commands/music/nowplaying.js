bc42fd5c6cb1dc7cc5e8799207db8e3bc3322000
BR88C <br88c@badfirmware.com>
2021-07-08 16:57:34 +0000

    build: TSC build for 926b1671acce81b64a2ef43e1413a48fe4873470

---
+++ dist/commands/music/nowplaying.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Constants_1 = require("../../config/Constants");
+const Time_1 = require("../../utils/Time");
+exports.default = {
+    command: `nowplaying`,
+    interaction: {
+        name: `nowplaying`,
+        description: `Get information on the current song playing.`
+    },
+    exec: (ctx) => {
+        const player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id);
+        if (!player || !player.queue?.current)
+            return void ctx.error(`Unable to get the current song; no music is playing.`);
+        let description;
+        if (player.queue.current.isStream)
+            description = `🔴  **LIVE**`;
+        else
+            description = `\`\`\`\n${player.paused ? `⏸` : `▶`} ${Time_1.timestamp(player.position)} ${Time_1.progressBar(player.position / (player.queue.current.duration ?? player.position), 25)} ${Time_1.timestamp(player.queue.current.duration ?? player.position)}`;
+        ctx.embed
+            .color(Constants_1.Constants.NOW_PLAYING_EMBED_COLOR)
+            .author(`Currently playing:`)
+            .title(player.queue.current.title, player.queue.current.uri)
+            .thumbnail(player.queue.current.displayThumbnail ? (player.queue.current.displayThumbnail(`mqdefault`) ?? ``) : ``)
+            .description(description)
+            .footer(`Requested by ${player.queue.current.requester}`)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+};
