bfe6d305879324ebf7fda8d7e086606f9703bbc8
BR88C <bcroethel88@gmail.com>
2021-07-08 12:15:17 -0400

    Create clear.ts

---
+++ src/commands/music/clear.ts
@@ -0,0 +1,25 @@
+import { Constants } from '../../config/Constants';
+import { ExtendedPlayer } from '../../managers/LavalinkManager';
+
+// Import modules.
+import { CommandOptions } from 'discord-rose';
+
+export default {
+    command: `clear`,
+    interaction: {
+        name: `clear`,
+        description: `Clears the queue.`
+    },
+    exec: (ctx) => {
+        const player: ExtendedPlayer | undefined = ctx.worker.lavalink.players.get(ctx.interaction.guild_id) as any;
+        if (!player) return void ctx.error(`Unable to clear the queue; no music is playing.`);
+
+        player.queue.clear();
+
+        ctx.embed
+            .color(Constants.QUEUE_CLEARED_EMBED_COLOR)
+            .title(`:broom:  Cleared the queue.`)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+} as CommandOptions;
