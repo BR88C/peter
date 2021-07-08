6ab387ee720a438c70bca01a2f7ac34be7517751
BR88C <br88c@badfirmware.com>
2021-07-08 16:41:24 +0000

    build: TSC build for 1a77bd7b997d74fa750747fe5999c75c3d58ed82

--- dist/utils/Time.js
+++ dist/utils/Time.js
@@ -1,14 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateTimestamp = exports.timestamp = void 0;
-const timestamp = (time) => {
-    time = Math.round(time / 1e3);
-    const hours = Math.floor(time / 60 / 60);
-    const minutes = Math.floor(time / 60) - (hours * 60);
-    const seconds = time % 60;
-    return hours > 0 ? `${hours.toString()}:${minutes.toString().padStart(2, `0`)}:${seconds.toString().padStart(2, `0`)}` : `${minutes.toString()}:${seconds.toString().padStart(2, `0`)}`;
-};
-exports.timestamp = timestamp;
+exports.timestamp = exports.progressBar = exports.dateTimestamp = void 0;
 const dateTimestamp = (time) => {
     const second = time.getSeconds().toString().padStart(2, `0`);
     const minute = time.getMinutes().toString().padStart(2, `0`);
@@ -19,3 +11,18 @@ const dateTimestamp = (time) => {
     return `${month}-${day}-${year} ${hour}:${minute}:${second}`;
 };
 exports.dateTimestamp = dateTimestamp;
+const progressBar = (percent, length) => {
+    if (length % 2 === 0)
+        throw new Error(`length must be an odd integer.`);
+    const lengthCompleted = Math.round(percent * (length - 1));
+    return `${`─`.repeat(lengthCompleted)}🔘${`─`.repeat((length - 1) - lengthCompleted)}`;
+};
+exports.progressBar = progressBar;
+const timestamp = (time) => {
+    time = Math.round(time / 1e3);
+    const hours = Math.floor(time / 60 / 60);
+    const minutes = Math.floor(time / 60) - (hours * 60);
+    const seconds = time % 60;
+    return hours > 0 ? `${hours.toString()}:${minutes.toString().padStart(2, `0`)}:${seconds.toString().padStart(2, `0`)}` : `${minutes.toString()}:${seconds.toString().padStart(2, `0`)}`;
+};
+exports.timestamp = timestamp;
