37eec7bf5d186264d91a0f7b2166359eb1e63e22
BR88C <br88c@badfirmware.com>
2021-07-08 16:35:44 +0000

    build: TSC build for 578cc7c4066ceae7ec7d92bbc8061a541af450f1

--- dist/utils/StringUtils.js
+++ dist/utils/StringUtils.js
@@ -1,7 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.centerString = exports.removeToken = exports.cleanseMarkdown = void 0;
+exports.removeToken = exports.cleanseMarkdown = exports.centerString = void 0;
 const discord_rose_1 = require("discord-rose");
+const centerString = (str, length) => {
+    if (str.length > length)
+        throw new discord_rose_1.CommandError(`Invalid length to center string on.`);
+    else
+        return `${` `.repeat(Math.floor((length - str.length) / 2))}${str}${` `.repeat(Math.ceil((length - str.length) / 2))}`;
+};
+exports.centerString = centerString;
 const cleanseMarkdown = (str) => str
     .replace(/`/g, `\\\``)
     .replace(/~/g, `\\-`)
@@ -11,10 +18,3 @@ const cleanseMarkdown = (str) => str
 exports.cleanseMarkdown = cleanseMarkdown;
 const removeToken = (str) => str.split(process.env.BOT_TOKEN ?? `%bot_token%`).join(`%bot_token%`);
 exports.removeToken = removeToken;
-const centerString = (str, length) => {
-    if (str.length > length)
-        throw new discord_rose_1.CommandError(`Invalid length to center string on.`);
-    else
-        return `${` `.repeat(Math.floor((length - str.length) / 2))}${str}${` `.repeat(Math.ceil((length - str.length) / 2))}`;
-};
-exports.centerString = centerString;
