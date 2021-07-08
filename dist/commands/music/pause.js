0483dcf79679417e564ddf86038fde0fa64937f9
BR88C <br88c@badfirmware.com>
2021-07-08 17:24:47 +0000

    build: TSC build for 597c8e6e296b6d28bf76171216c69961dd857cb1

---
+++ dist/commands/music/pause.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Constants_1 = require("../../config/Constants");
+exports.default = {
+    command: `pause`,
+    interaction: {
+        name: `pause`,
+        description: `Pause the music.`
+    },
+    exec: (ctx) => {
+        const player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id);
+        if (!player || !player.queue?.current)
+            return void ctx.error(`Unable to disconnect the bot; no music is playing.`);
+        if (player.paused)
+            return void ctx.error(`The music is already paused.`);
+        player.pause(true);
+        ctx.embed
+            .color(Constants_1.Constants.PAUSE_EMBED_COLOR)
+            .title(`:pause_button:  Paused the music`)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+};
