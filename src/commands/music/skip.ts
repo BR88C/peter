1c09f9a54792b51ad86eb0dcf12a4bd953cd7a7b
BR88C <bcroethel88@gmail.com>
2021-07-18 23:42:01 -0400

    Create skip.ts

---
+++ src/commands/music/skip.ts
@@ -0,0 +1,34 @@
+import { Constants } from '../../config/Constants';
+
+// Import modules.
+import { CommandOptions } from 'discord-rose';
+import { Player } from '@discord-rose/lavalink';
+
+export default {
+    command: `skip`,
+    interaction: {
+        name: `skip`,
+        description: `Skip to the next song, or to a specified song.`,
+        options: [
+            {
+                type: 4,
+                name: `index`,
+                description: `The song index to skip to.`,
+                required: false
+            }
+        ]
+    },
+    exec: async (ctx) => {
+        const player: Player | undefined = ctx.worker.lavalink.players.get(ctx.interaction.guild_id) as any;
+        if (!player || !player.queue.length) return void ctx.error(`Unable to skip; there is no music in the queue.`);
+
+        const index = typeof ctx.options.index?.value === `number` ? ctx.options.index.value - 1 : undefined;
+        await player.skip(index);
+
+        ctx.embed
+            .color(Constants.SKIP_EMBED_COLOR)
+            .title(`:track_next:  Skipped to the next track`)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+} as CommandOptions;
