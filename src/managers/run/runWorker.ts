f3522a081c4bcb52b436e7ba2459f0cffe17ca33
BR88C <bcroethel88@gmail.com>
2021-07-20 12:06:48 -0400

    Extend the player

--- src/managers/run/runWorker.ts
+++ src/managers/run/runWorker.ts
@@ -1,7 +1,17 @@
 import { WorkerManager } from '../WorkerManager';
 
+import { Player as BasePlayer } from '@discord-rose/lavalink';
+
 void new WorkerManager();
 
 declare module 'discord-rose/dist/typings/lib' { // eslint-disable-line quotes
     type worker = WorkerManager
 };
+
+class ExtendedPlayer extends BasePlayer {
+    twentyfourseven: boolean
+}
+
+declare module '@discord-rose/lavalink/dist/typings/lib' { // eslint-disable-line quotes
+    type player = ExtendedPlayer
+}
