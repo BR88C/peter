2608489b70cb4b2e551547731e63c8c2310dcd82
BR88C <br88c@br88c.dev>
2021-07-22 19:33:45 -0400

    Create remove.ts

---
+++ src/commands/music/remove.ts
@@ -0,0 +1,39 @@
+import { Constants } from '../../config/Constants';
+
+// Import modules.
+import { CommandOptions } from 'discord-rose';
+
+export default {
+    command: `remove`,
+    interaction: {
+        name: `remove`,
+        description: `Remove a track from the queue.`,
+        options: [
+            {
+                type: 4,
+                name: `index`,
+                description: `The track's index in the queue.`,
+                required: true
+            }
+        ]
+    },
+    exec: async (ctx) => {
+        const player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id!);
+        if (!player || !player.queue.length) return void ctx.error(`Unable to remove a track from the queue; there are no tracks in the queue.`);
+
+        const foundVoiceState = ctx.worker.voiceStates.find((state) => state.guild_id === ctx.interaction.guild_id && state.users.has(ctx.author.id));
+        if (!foundVoiceState || foundVoiceState.channel_id !== player.options.voiceChannelId) return void ctx.error(`You must be in the VC to remove tracks from the queue.`);
+
+        if (ctx.options.index < 1 || ctx.options.index > player.queue.length) return void ctx.error(`Please specify a valid index.`);
+
+        const removedTrack = player.queue.splice(ctx.options.index - 1, 1)[0];
+
+        await ctx.embed
+            .color(Constants.REMOVED_TRACK_EMBED_COLOR)
+            .title(`:x:  Removed "${removedTrack.title}" from the queue`)
+            .send()
+            .catch((error) => void ctx.error(error));
+
+        if (ctx.options.index - 1 === player.queuePosition) void player.skip(player.queuePosition);
+    }
+} as CommandOptions;
