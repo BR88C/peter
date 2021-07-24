e7fe39843fde436c946833c420f2ea1ccf19904a
BR88C <br88c@br88c.dev>
2021-07-23 23:09:15 -0400

    Update Guild.ts

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -12,6 +12,7 @@ const GuildSchema: Schema = new Schema({
     /**
      * DJ role mode.
      * 0 = No DJ role, use normal behavior.
+     * 1 = Non-DJs can do everything except manage effects, and only DJs can skip (non-DJs can voteskip)
      * 1 = Non-DJs can only add tracks, voteskip, and view the queue and the current track playing.
      * 2 = Non-DJs cannot do anything except view the queue and the current track playing.
      */
