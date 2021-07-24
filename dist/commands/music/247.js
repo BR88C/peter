f269756bd0954d30169919c996ef6911b49341b1
BR88C <br88c@badfirmware.com>
2021-07-24 03:17:52 +0000

    build: TSC build for 0458958a0c29618c969e04bd4cb002b745bb29da

---
+++ dist/commands/music/247.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Constants_1 = require("../../config/Constants");
+exports.default = {
+    command: `247`,
+    interaction: {
+        name: `247`,
+        description: `Toggle 24/7.`
+    },
+    exec: (ctx) => {
+        const player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id);
+        if (!player)
+            return void ctx.error(`Unable to set the queue to 24/7; the bot is not connected to the VC.`);
+        const foundVoiceState = ctx.worker.voiceStates.find((state) => state.guild_id === ctx.interaction.guild_id && state.users.has(ctx.author.id));
+        if (foundVoiceState?.channel_id !== player.options.voiceChannelId)
+            return void ctx.error(`You must be in the VC to set the queue to 24/7.`);
+        player.twentyfourseven = !player.twentyfourseven;
+        ctx.embed
+            .color(Constants_1.Constants.TWENTY_FOUR_SEVEN_EMBED_COLOR)
+            .title(`:clock2:  24/7 is now \`${player.twentyfourseven ? `On` : `Off`}\``)
+            .send()
+            .catch((error) => void ctx.error(error));
+    }
+};
