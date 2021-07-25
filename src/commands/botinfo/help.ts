70a646496bdb1dfb86ecd1a2adbb21b72647f7cc
BR88C <br88c@br88c.dev>
2021-07-25 15:35:27 -0400

    Update help.ts

--- src/commands/botinfo/help.ts
+++ src/commands/botinfo/help.ts
@@ -1,20 +1,49 @@
+import { Config } from '../../config/Config';
 import { Constants } from '../../config/Constants';
 
 // Import modules.
-import { CommandOptions } from 'discord-rose';
+import { CommandOptions, Embed } from 'discord-rose';
 
 export default {
     command: `help`,
     interaction: {
         name: `help`,
-        description: `Get help using the bot.`
+        description: `Get help using the bot.`,
+        options: [
+            {
+                type: 3,
+                name: `command`,
+                description: `The name of the command.`,
+                required: false
+            }
+        ]
     },
     exec: (ctx) => {
-        ctx.embed
-            .color(Constants.INVITE_EMBED_COLOR)
-            .title(`Join our support server to get help!`)
-            .description(Constants.SUPPORT_SERVER)
-            .send()
-            .catch((error) => void ctx.error(error));
+        if (!ctx.worker.commands.commands) return void ctx.error(`Unable to get commands.`);
+
+        if (ctx.options.command) {
+            const command = ctx.worker.commands.commands.find((command) => command.interaction?.name.toLocaleLowerCase() === ctx.options.command.toLowerCase());
+            if (!command || !command.interaction) return void ctx.error(`Invalid command.`);
+            ctx.embed
+                .color(Constants.HELP_EMBED_COLOR)
+                .title(`Command information`)
+                .description(`**Name:** ${command.interaction.name}\n**Description:** ${command.interaction.description}`)
+                .footer(`Peter! made by ${Config.devs.tags.join(`, `)}`)
+                .timestamp()
+                .send()
+                .catch((error) => void ctx.error(error));
+        } else {
+            // @ts-expect-errorProperty 'category' does not exist on type 'CommandOptions'.
+            const categories = [...new Set(ctx.worker.commands.commands.map((command) => command.category.toLowerCase()))].map((category) => ctx.worker.commands.commands.filter((command) => command.interaction && command.category === category)).sort((a, b) => a.size - b.size);
+            const helpEmbed = new Embed()
+                .color(Constants.HELP_EMBED_COLOR)
+                .title(`Help`)
+                .description(`Peter's support server: ${Constants.SUPPORT_SERVER}`)
+                .footer(`Peter! made by ${Config.devs.tags.join(`, `)}`)
+                .timestamp();
+            // @ts-expect-errorProperty 'category' does not exist on type 'CommandOptions'.
+            for (const category of categories) helpEmbed.field(`${category.first().category.charAt(0).toUpperCase()}${category.first().category.slice(1)}`, category.map((command) => `\`${command.interaction.name}\``).join(`, `), true);
+            ctx.send(helpEmbed).catch((error) => void ctx.error(error));
+        }
     }
 } as CommandOptions;
