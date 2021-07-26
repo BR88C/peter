dc6ef6412eca4dea59e3990de44fc7af3677da97
BR88C <br88c@br88c.dev>
2021-07-25 21:41:08 -0400

    Update serverconfig.ts

--- src/commands/config/serverconfig.ts
+++ src/commands/config/serverconfig.ts
@@ -1,4 +1,5 @@
 import { Config } from '../../config/Config';
+import { Constants } from '../../config/Constants';
 
 // Import modules.
 import { Collection } from '@discordjs/collection';
@@ -35,21 +36,42 @@ export default {
                     roleList: guild.roles.reduce((p, c) => p.set(c.id, c), new Collection()) as any
                 }), `manageGuild`)) return void ctx.error(`You must have the "Manage Server" permission to set DJ commands.`);
 
+                if (!ctx.worker.commands.commands) return void ctx.error(`Unable to get commands.`);
                 // @ts-expect-error Property 'category' does not exist on type 'CommandOptions'.
-                if (!ctx.worker.commands.commands?.find((command) => command.category === `music` && command.interaction?.name.toLowerCase() === ctx.options.dj.command.toLowerCase())) return void ctx.error(`Invalid command.`);
+                if (!ctx.worker.commands.commands.find((command) => command.category === `music` && command.interaction?.name.toLowerCase() === ctx.options.dj.command.toLowerCase())) return void ctx.error(`Invalid command.`);
 
-                const guildDocument = await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guild`).findOne({ id: ctx.interaction.guild_id });
+                const guildDocument = await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guilds`).findOne({ id: ctx.interaction.guild_id });
                 if (!guildDocument) {
-                    await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guild`).insertOne({
+                    await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guilds`).insertOne({
                         id: ctx.interaction.guild_id,
                         premium: false,
                         djCommands: [ctx.options.dj.command.toLowerCase()]
                     });
                 } else {
-                    await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guild`).updateOne({ id: ctx.interaction.guild_id }, { $set: { djCommands: guildDocument.djCommands } });
+                    const newArray = guildDocument.djCommands.includes(ctx.options.dj.command.toLowerCase()) ? guildDocument.djCommands.filter((command) => command !== ctx.options.dj.command.toLowerCase()) : guildDocument.djCommand.concat(ctx.options.dj.command.toLowerCase());
+                    await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guilds`).updateOne({ id: ctx.interaction.guild_id }, { $set: { djCommands: newArray } });
                 }
-            } else {
 
+                ctx.embed
+                    .color(Constants.CONFIG_EMBED_COLOR)
+                    .title(`Toggled the \`${ctx.options.dj.command.toLowerCase()}\` command to be \`${guildDocument?.djCommands.includes(ctx.options.dj.command.toLowerCase()) ? `Public` : `DJ only`}\``)
+                    .send(true, false, true)
+                    .catch((error) => void ctx.error(error));
+            } else {
+                if (!ctx.worker.commands.commands) return void ctx.error(`Unable to get commands.`);
+                const guildDocument = await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guilds`).findOne({ id: ctx.interaction.guild_id });
+                // @ts-expect-error Property 'category' does not exist on type 'CommandOptions'.
+                const commands = ctx.worker.commands.commands.filter((command) => command.interaction && command.category === `music`).map((command) => `${guildDocument?.djCommands.includes(command.interaction.name) ? `:lock:` : `:unlock:`} \`${command.interaction.name}\``);
+
+                ctx.embed
+                    .color(Constants.CONFIG_EMBED_COLOR)
+                    .title(`DJ Config`)
+                    .description(`Peter's DJ configuration works by toggling individual commands to be public or DJ only, allowing for total control over what non-DJs can and cannot access. To make someone a DJ, simply give them a role named "DJ". You can create as many DJ roles as you would like.`)
+                    .field(`Current Configuration`, commands.splice(0, Math.ceil(commands.length / 2)).join(`\n`), true)
+                    .field(`\u200b`, commands.join(`\n`), true)
+                    .footer(`🔒 = DJ only, 🔓 = Public`)
+                    .send(true, false, true)
+                    .catch((error) => void ctx.error(error));
             }
         }
     }
