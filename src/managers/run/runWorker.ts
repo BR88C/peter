79e66b0d3868dcb70a1aa2552ecb48075158da6b
BR88C <br88c@br88c.dev>
2021-07-31 11:09:49 -0400

    Add comments

--- src/managers/run/runWorker.ts
+++ src/managers/run/runWorker.ts
@@ -7,11 +7,17 @@ void new WorkerManager();
 declare module 'discord-rose/dist/typings/lib' { // eslint-disable-line quotes
     type worker = WorkerManager
     interface CommandOptions {
+        /**
+         * The command's category.
+         */
         category: string
     }
 }
 
 export class ExtendedPlayer extends BasePlayer {
+    /**
+     * If the player should stay in the VC after all users leave.
+     */
     twentyfourseven: boolean
 }
 
