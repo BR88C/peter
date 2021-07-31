fb152c37783da8b9fc8c22a0a217be34811076d1
BR88C <br88c@br88c.dev>
2021-07-31 18:56:31 -0400

    Implement new command handler middleware with 24/7

--- src/commands/music/247.ts
+++ src/commands/music/247.ts
@@ -2,34 +2,21 @@ import { Constants } from '../../config/Constants';
 
 // Import modules.
 import { CommandOptions } from 'discord-rose';
-import { PlayerState } from '@discord-rose/lavalink';
 
 export default {
     command: `247`,
+    mustHaveConnectedPlayer: true,
+    userMustBeInSameVC: true,
     interaction: {
         name: `247`,
         description: `Toggle 24/7.`
     },
     exec: async (ctx) => {
-        const player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id!);
-        if (!player || player.state < PlayerState.CONNECTED) return void ctx.error(`Unable to set the queue to 24/7; the bot is not connected to a voice channel.`);
-
-        const foundVoiceState = ctx.worker.voiceStates.find((state) => state.guild_id === ctx.interaction.guild_id && state.users.has(ctx.author.id));
-        if (foundVoiceState?.channel_id !== player.options.voiceChannelId) return void ctx.error(`You must be in the voice channel to set the queue to 24/7.`);
-
-        // @ts-expect-error Argument of type '"GET_VOTE"' is not assignable to parameter of type 'keyof ThreadEvents'.
-        if (!(await ctx.worker.comms.sendCommand(`GET_VOTE`, { user_id: ctx.author.id }))) return ctx.embed
-            .color(Constants.ERROR_EMBED_COLOR)
-            .title(`You must vote to use this command! Please vote by going to the link below.`)
-            .description(Constants.VOTE_LINK)
-            .send(true, false, true)
-            .catch(() => void ctx.error(`Unable to send the response message.`));
-
-        player.twentyfourseven = !player.twentyfourseven;
+        ctx.player!.twentyfourseven = !ctx.player!.twentyfourseven;
 
         ctx.embed
             .color(Constants.TWENTY_FOUR_SEVEN_EMBED_COLOR)
-            .title(`:clock2:  24/7 is now \`${player.twentyfourseven ? `On` : `Off`}\``)
+            .title(`:clock2:  24/7 is now \`${ctx.player!.twentyfourseven ? `On` : `Off`}\``)
             .send()
             .catch(() => void ctx.error(`Unable to send the response message.`));
     }
