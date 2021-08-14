28241608557bf87dc5a5266c0303f1c1512da7d4
BR88C <br88c@br88c.dev>
2021-08-14 14:28:23 -0400

    Revert

--- src/managers/WorkerManager.ts
+++ src/managers/WorkerManager.ts
@@ -1,17 +1,17 @@
 import { bindLavalinkEvents } from '../utils/Lavalink';
 import { Config } from '../config/Config';
 import { Constants } from '../config/Constants';
+import { Middleware } from '../utils/Middleware';
 import { Presences } from '../config/Presences';
 
 // Import modules.
-import { Collection } from '@discordjs/collection';
-import { loadCommands, logError, removeToken, setRandomPresence } from '@br88c/discord-utils';
+import { errorFunction, loadCommands, logError, setRandomPresence } from '@br88c/discord-utils';
 import { Agent as HttpAgent } from 'http';
 import { Agent as HttpsAgent } from 'https';
-import { LavalinkManager, PlayerState } from '@discord-rose/lavalink';
+import { LavalinkManager } from '@discord-rose/lavalink';
 import { MongoClient } from 'mongodb';
-import { PermissionsUtils, Worker } from 'discord-rose';
 import { resolve } from 'path';
+import { Worker } from 'discord-rose';
 
 /**
  * The Worker manager class.
@@ -88,96 +88,7 @@ export class WorkerManager extends Worker {
         });
 
         // Create command middleware.
-        this.commands.middleware(async (ctx) => {
-            if (!ctx.worker.available) { // If the worker is not available.
-                void ctx.error(`The bot is still starting; please wait!`);
-                return false;
-            }
-            if (!ctx.isInteraction) { // If the received event is not an interaction.
-                if (!Config.devs.IDs.includes(ctx.author.id)) { // If the user is not a dev, return an error.
-                    void ctx.error(`Peter's prefix commands (sudo) have been replaced by slash commands. For more information, join our support server!`);
-                    return false;
-                } else { // If the user is a dev.
-                    if (ctx.command.interaction) { // If the command is a slash command, return.
-                        void ctx.error(`That's an interaction command, not a developer command silly!`);
-                        return false;
-                    } else { // If the command is not a slash command, execute it.
-                        ctx.worker.log(`\x1b[32mReceived Dev Command | Command: ${ctx.command.command} | User: ${ctx.author.username}#${ctx.author.discriminator}${ctx.message.guild_id ? ` | Guild ID: ${ctx.message.guild_id}` : ``}`);
-                        return true;
-                    }
-                }
-            } else { // If the received event is an interaction.
-                ctx.player = ctx.worker.lavalink.players.get(ctx.interaction.guild_id!);
-                ctx.voiceState = ctx.worker.voiceStates.find((state) => state.guild_id === ctx.interaction.guild_id && state.users.has(ctx.author.id));
-                // @ts-expect-error This condition will always return 'true' since the types 'InteractionType.ApplicationCommand' and '3' have no overlap.
-                if (!ctx.command.allowButton && ctx.interaction.type === 3) {
-                    void ctx.error(`An internal button error occured. Please submit an issue in our support server.`);
-                    return false;
-                }
-                if (ctx.command.mustBePaused && ctx.player?.state !== PlayerState.PAUSED) {
-                    void ctx.error(`The music must be paused to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.mustBePausedOrPlaying && (ctx.player?.state ?? 0) < PlayerState.PAUSED) {
-                    void ctx.error(`The music must be paused or playing to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.mustBePlaying && (ctx.player?.state !== PlayerState.PLAYING || ctx.player?.queuePosition === null || !ctx.player?.currentTrack)) {
-                    void ctx.error(`The bot must be playing music to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.mustHaveConnectedPlayer && (ctx.player?.state ?? 0) < PlayerState.CONNECTED) {
-                    void ctx.error(`The bot must be connected to the voice channel to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.mustHavePlayer && !ctx.player) {
-                    void ctx.error(`The bot must be connecting or connected to the voice channel to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.mustHaveTracksInQueue && !ctx.player?.queue.length) {
-                    void ctx.error(`There must be music in the queue to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.userMustBeInSameVC && (!ctx.player || ctx.voiceState?.channel_id !== ctx.player.options.voiceChannelId)) {
-                    void ctx.error(`You must be in the same voice channel as the bot to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.userMustBeInVC && !ctx.voiceState) {
-                    void ctx.error(`You must be in a voice channel to run the "${ctx.command.interaction!.name}" command.`);
-                    return false;
-                }
-                if (ctx.command.voteLocked && !(await ctx.worker.comms.sendCommand(`CHECK_VOTE`, ctx.author.id))) {
-                    await ctx.embed
-                        .color(Constants.ERROR_EMBED_COLOR)
-                        .title(`You must vote to use this command! Please vote by going to the link below.`)
-                        .description(Constants.VOTE_LINK)
-                        .send(true, false, true)
-                        .catch(() => void ctx.error(`Unable to send the response message.`));
-                    return false;
-                }
-        
-                if (ctx.command.category === `music`) { // If the interaction is a music command.
-                    const guildDocument = await ctx.worker.mongoClient.db(Config.mongo.dbName).collection(`Guilds`).findOne({ id: ctx.interaction.guild_id });
-                    if (guildDocument?.djCommands.includes(ctx.command.interaction!.name.toLowerCase())) {
-                        const voiceChannel = ctx.worker.lavalink.players.get(ctx.interaction.guild_id!)?.options.voiceChannelId ?? ctx.voiceState?.channel_id;
-                        if (voiceChannel && (ctx.worker.voiceStates.get(voiceChannel)?.users.size ?? 1) - 1 >= guildDocument.djOverride) {
-                            const guild = await ctx.worker.api.guilds.get(ctx.interaction.guild_id!);
-                            if (!PermissionsUtils.has(PermissionsUtils.combine({
-                                guild,
-                                member: ctx.interaction.member!,
-                                roleList: guild.roles.reduce((p, c) => p.set(c.id, c), new Collection()) as any
-                            }), `manageGuild`) && !guild.roles.filter((role) => role.name.toLowerCase() === `dj`).map((role) => role.id).some((role) => ctx.interaction.member!.roles.includes(role))) {
-                                void ctx.error(`You must have the DJ role to use that command.`);
-                                return false;
-                            }
-                        }
-                    }
-                }
-        
-                ctx.worker.log(`Received Interaction | Command: ${ctx.ran} | User: ${ctx.author.username}#${ctx.author.discriminator} | Guild ID: ${ctx.interaction.guild_id}`);
-                return true;
-            }
-        });
+        this.commands.middleware(async (ctx) => Middleware(ctx));
 
         // On ready.
         this.on(`READY`, async () => {
