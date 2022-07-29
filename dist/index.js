c09362fcf84c1e715b1a44ef6ce9ad32bdb34900
BR88C <br88c@br88c.dev>
2022-07-29 03:39:09 +0000

    build: TSC build for 91f217fee471f69ab6c5410097247abfe683ce57

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ClientManager_1 = require("./structures/ClientManager");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-if (typeof process.env.BOT_TOKEN !== `string`)
-    throw new Error(`BOT_TOKEN must be defined`);
-if (typeof process.env.LAVALINK_HOST !== `string`)
-    throw new Error(`LAVALINK_HOST must be defined`);
-if (typeof process.env.LAVALINK_PORT !== `string` || isNaN(parseInt(process.env.LAVALINK_PORT)))
-    throw new Error(`LAVALINK_PORT must be defined`);
-if (typeof process.env.LAVALINK_SECURE !== `string`)
-    throw new Error(`LAVALINK_SECURE must be defined`);
-if (typeof process.env.LAVALINK_PASSWORD !== `string`)
-    throw new Error(`LAVALINK_PASSWORD must be defined`);
+const tokens_1 = require("./utils/tokens");
+(0, tokens_1.loadTokens)();
 const clientManager = new ClientManager_1.ClientManager();
 if (process.env.NODE_ENV === `dev`)
     global.clientManager = clientManager;
--- dist/structures/ClientManager.js
+++ dist/structures/ClientManager.js
@@ -3,20 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ClientManager = void 0;
 const Lavalink_1 = require("./Lavalink");
 const Constants_1 = require("../utils/Constants");
-const tokenFilters_1 = require("../utils/tokenFilters");
-const node_utils_1 = require("@br88c/node-utils");
-const cmd_1 = require("@distype/cmd");
+const tokens_1 = require("../utils/tokens");
+const distype_boilerplate_1 = require("@br88c/distype-boilerplate");
 const lavalink_1 = require("@distype/lavalink");
-const v10_1 = require("discord-api-types/v10");
-const distype_1 = require("distype");
+const influxdb_client_1 = require("@influxdata/influxdb-client");
 const node_path_1 = require("node:path");
-class ClientManager extends distype_1.Client {
+class ClientManager extends distype_boilerplate_1.ClientManager {
     constructor() {
-        const logger = new node_utils_1.Logger({
-            enabledOutput: { log: [`DEBUG`, `INFO`, `WARN`, `ERROR`] },
-            sanitizeTokens: (0, tokenFilters_1.tokenFilters)()
-        });
-        super(process.env.BOT_TOKEN, {
+        super(process.env.BOT_TOKEN, (0, tokens_1.tokenFilters)(), {
+            influxDB: {
+                application: `peter`,
+                bucket: process.env.INFLUX_BUCKET,
+                org: process.env.INFLUX_ORG,
+                token: process.env.INFLUX_TOKEN,
+                url: process.env.INFLUX_URL,
+                reportInterval: 10000
+            },
+            topgg: process.env.TOPGG_TOKEN?.length ? {
+                postShards: true,
+                reportInterval: 1800000
+            } : undefined
+        }, {
             cache: {
                 channels: [`name`, `permission_overwrites`, `type`],
                 guilds: [`owner_id`, `roles`, `unavailable`],
@@ -25,8 +32,7 @@ class ClientManager extends distype_1.Client {
                 voiceStates: [`channel_id`]
             },
             gateway: { intents: [`GUILDS`, `GUILD_VOICE_STATES`] }
-        }, logger.log, logger);
-        this.commandHandler = new cmd_1.CommandHandler(this, logger.log, logger);
+        });
         this.lavalink = new Lavalink_1.Lavalink(this, {
             clientName: `peter@${process.env.npm_package_version ?? `0.0.0`}`,
             nodeOptions: [
@@ -39,44 +45,21 @@ class ClientManager extends distype_1.Client {
                     password: process.env.LAVALINK_PASSWORD
                 }
             ]
-        }, logger.log, logger);
-        this.logger = logger;
+        }, this.logger.log, this.logger);
+        this.metrics.setInfluxDBCallback(() => {
+            const lavalinkPoints = [];
+            this.lavalink.nodes.forEach((node) => {
+                lavalinkPoints.push(new influxdb_client_1.Point(`lavalinkNode`)
+                    .tag(`id`, `${node.id}`)
+                    .floatField(`cpu`, node.stats.cpu.lavalinkLoad)
+                    .intField(`memory`, node.stats.memory.used)
+                    .intField(`players`, node.stats.players)
+                    .intField(`activePlayers`, node.stats.playingPlayers));
+            });
+            return lavalinkPoints;
+        });
     }
     async init() {
-        this.commandHandler
-            .setError(async (ctx, error, unexpected) => {
-            const errorId = `${Math.round(Math.random() * 1e6).toString(36).padStart(5, `0`)}${Date.now().toString(36)}`.toUpperCase();
-            this.logger.log(`${unexpected ? `Unexpected ` : ``}${error.name} (ID: ${errorId}) when running interaction ${ctx.interaction.id}: ${error.message}`, {
-                level: `ERROR`, system: `Command Handler`
-            });
-            if (unexpected) {
-                console.error(`\n${node_utils_1.LoggerRawFormats.RED}${error.stack}${node_utils_1.LoggerRawFormats.RESET}\n`);
-            }
-            const tokenFilter = (0, tokenFilters_1.tokenFilters)({
-                token: ctx.interaction.token,
-                replacement: `%interaction_token%`
-            });
-            const errorEmbed = new cmd_1.Embed()
-                .setColor(cmd_1.DiscordColors.BRANDING_RED)
-                .setTitle(`Error`)
-                .setDescription(`\`\`\`\n${(0, node_utils_1.sanitizeTokens)(error.message, tokenFilter)}\n\`\`\`\n*Support Server: ${process.env.SUPPORT_SERVER?.length ? process.env.SUPPORT_SERVER : `\`Support Server Unavailable\``}*`);
-            if (unexpected) {
-                errorEmbed
-                    .setFooter(`Error ID: ${errorId}`)
-                    .setTimestamp();
-            }
-            await ctx.sendEphemeral(errorEmbed);
-        })
-            .setExpireError((ctx, error, unexpected) => {
-            this.logger.log(`${unexpected ? `Unexpected ` : ``}${error.name} when running expire callback for component "${ctx.component.customId}" (${v10_1.ComponentType[ctx.component.type]})`, {
-                level: `ERROR`, system: `Command Handler`
-            });
-            if (unexpected) {
-                console.error(`\n${node_utils_1.LoggerRawFormats.RED}${error.stack}${node_utils_1.LoggerRawFormats.RESET}\n`);
-            }
-        });
-        await this.commandHandler.load((0, node_path_1.resolve)(__dirname, `../commands`));
-        await this.commandHandler.load((0, node_path_1.resolve)(__dirname, `../contextcommands`));
         this.gateway.on(`VOICE_STATE_UPDATE`, ({ d }) => {
             if (!d.guild_id)
                 return;
@@ -96,39 +79,9 @@ class ClientManager extends distype_1.Client {
                 player.voiceTimeout = null;
             }
         });
-        if (process.env.TOPGG_TOKEN?.length) {
-            setInterval(() => {
-                this.topggRequest(`POST`, `/bots/stats`, { body: {
-                        server_count: this.cache.guilds?.size,
-                        shard_count: this.gateway.shards.size
-                    } })
-                    .then(() => this.logger.log(`Posted stats to Top.gg`, { system: `Top.gg` }))
-                    .catch((error) => {
-                    this.logger.log(error.name, {
-                        level: `ERROR`, system: `Top.gg`
-                    });
-                    console.error(`\n${node_utils_1.LoggerRawFormats.RED}${error.stack}${node_utils_1.LoggerRawFormats.RESET}\n`);
-                });
-            }, Constants_1.Constants.TOPGG_POST_INTERVAL).unref();
-        }
-        else {
-            this.logger.log(`No Top.gg token provided, skipping initialization`, {
-                level: `WARN`, system: `Top.gg`
-            });
-        }
-        await this.gateway.connect();
+        this.setErrorCallbacks(process.env.SUPPORT_SERVER);
+        await super.init((0, node_path_1.resolve)(__dirname, `../commands`), (0, node_path_1.resolve)(__dirname, `../contextcommands`));
         await this.lavalink.spawnNodes();
-        await this.commandHandler.push();
-    }
-    async topggRequest(method, route, options) {
-        if (!process.env.TOPGG_TOKEN?.length)
-            throw new Error(`TOPGG_TOKEN is undefined`);
-        return (await this.rest.make(method, route, {
-            authHeader: process.env.TOPGG_TOKEN,
-            customBaseURL: `https://top.gg/api`,
-            forceHeaders: true,
-            ...options
-        })).body;
     }
 }
 exports.ClientManager = ClientManager;
